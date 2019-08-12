2ddc70b8c9831c407e11c85dcd2a630f5c2f6908
Joel Hawksley <joel@hawksley.org>
2019-08-12 13:36:33 -0600

    cleanup unnecessary rails files

--- test/config/application.rb
+++ test/config/application.rb
@@ -3,8 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require 'active_model/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
-#require 'active_record/railtie'
-#require 'action_mailer/railtie'
 require "sprockets/railtie"
 
 require "haml"
@@ -36,26 +34,5 @@ module Dummy
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
-    # From slim-rails fix for "ActionView::Template::Error: Unknown line indicator"
-    # https://github.com/slim-template/slim-rails/blob/991589ea5648e5e896781e68912bc51beaf4102a/lib/slim-rails/register_engine.rb
-    if config.respond_to?(:assets)
-      config.assets.configure do |env|
-        if env.respond_to?(:register_transformer) && Sprockets::VERSION.to_i > 3
-          env.register_mime_type 'text/slim', extensions: ['.slim', '.slim.html']
-          env.register_transformer 'text/slim', 'text/html', RegisterEngine::Transformer
-        elsif env.respond_to?(:register_engine)
-          args = ['.slim', Slim::Template]
-          args << { silence_deprecation: true } if Sprockets::VERSION.start_with?('3')
-          env.register_engine(*args)
-        end
-      end
-    end
   end
 end
