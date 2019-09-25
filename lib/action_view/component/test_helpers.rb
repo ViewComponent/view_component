5ea15c91ead946326a4c94910c5210793b35ce02
Patrick Sinclair <metade@gmail.com>
2019-09-25 11:07:46 +0100

    Extract component into a separate method in the test helper
    This will allow controller methods to be stubbed from tests.

--- lib/action_view/component/test_helpers.rb
+++ lib/action_view/component/test_helpers.rb
@@ -4,9 +4,11 @@ module ActionView
   module Component
     module TestHelpers
       def render_inline(component, **args, &block)
-        controller = ApplicationController.new
-        controller.request = ActionDispatch::TestRequest.create
         Nokogiri::HTML(controller.view_context.render(component, args, &block))
+      end
+
+      def controller
+        @controller ||= ApplicationController.new.tap { |c| c.request = ActionDispatch::TestRequest.create }
       end
 
       def render_component(component, **args, &block)
