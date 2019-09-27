5b460263e5bd45baed27355bfc5d00e531f225ab
Joel Hawksley <joel@hawksley.org>
2019-09-27 14:05:03 -0600

    add test coverage for URL helpers

--- test/action_view/component_test.rb
+++ test/action_view/component_test.rb
@@ -118,6 +118,12 @@ class ActionView::ComponentTest < Minitest::Test
     assert_includes result.text, "/"
   end
 
+  def test_renders_url_helper
+    result = render_inline(UrlComponent)
+
+    assert_includes result.text, "/"
+  end
+
   def test_template_changes_are_not_reflected_in_production
     ActionView::Base.cache_template_loading = true
 
