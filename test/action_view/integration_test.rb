8be65387aa8a8ba3033a863a526cefe7b34125f7
Joel Hawksley <joel@hawksley.org>
2019-11-04 13:06:38 -0700

    simplify test name

--- test/action_view/integration_test.rb
+++ test/action_view/integration_test.rb
@@ -29,7 +29,7 @@ class IntegrationTest < ActionDispatch::IntegrationTest
     assert_equal trim_result(response.body), "partial:<div>hello,partialworld!</div>component:<div>hello,partialworld!</div><div>hello,partialworld!</div>"
   end
 
-  test "rendering component in a view with deprecated syntax" do
+  test "rendering component with deprecated syntax" do
     get "/deprecated"
     assert_response :success
     assert_equal trim_result(response.body), "<span><div>Foobar</div></span>"
