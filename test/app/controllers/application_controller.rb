5f26e2cd65a302053941b5d17f5a50e7a4455288
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-11-07 23:03:48 -0300

    Disabling layout in application controller from test app

--- test/app/controllers/application_controller.rb
+++ test/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  layout false
+
   def variants
     request.variant = params[:variant].to_sym if params[:variant]
   end
