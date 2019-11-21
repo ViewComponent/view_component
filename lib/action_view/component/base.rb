421ab55f0f293d9d29d71318034dbd8497f9f696
Ryan Workman <rdavid1099@users.noreply.github.com>
2019-11-21 11:23:50 -0800

    Fix issue with extracting variants when source_location includes more '.'

--- lib/action_view/component/base.rb
+++ lib/action_view/component/base.rb
@@ -172,10 +172,12 @@ module ActionView
         def templates
           @templates ||=
             (Dir["#{source_location.sub(/#{File.extname(source_location)}$/, '')}.*{#{ActionView::Template.template_handler_extensions.join(',')}}"] - [source_location]).each_with_object([]) do |path, memo|
+              pieces = File.basename(path).split(".")
+
               memo << {
                 path: path,
-                variant: path.split(".").second.split("+")[1]&.to_sym,
-                handler: path.split(".").last
+                variant: pieces.second.split("+")[1]&.to_sym,
+                handler: pieces.last
               }
             end
         end
