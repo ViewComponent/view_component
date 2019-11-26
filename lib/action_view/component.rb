22381c08999e55c8b44a6e9a4ad595655e83cf02
Vinicius Stock <vinicius.stock@shopify.com>
2019-11-26 13:39:01 -0500

    Rename monkey patch

--- lib/action_view/component.rb
+++ lib/action_view/component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 require "action_view/component/monkey_patch"
-require "action_view/component/am_monkey_patch"
+require "action_view/component/active_model_conversion_monkey_patch"
 require "action_view/component/base"
 require "action_view/component/railtie"
