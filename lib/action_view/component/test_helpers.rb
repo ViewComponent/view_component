ac007ed07db40f42ee6c498922ea4addf39646a4
inatomitk <35132611+inatomitk@users.noreply.github.com>
2019-11-26 16:31:37 +0900

    Remove deprecated test helper method "render_component"

--- lib/action_view/component/test_helpers.rb
+++ lib/action_view/component/test_helpers.rb
@@ -15,14 +15,6 @@ module ActionView
         @request ||= ActionDispatch::TestRequest.create
       end
 
-      def render_component(component, **args, &block)
-        ActiveSupport::Deprecation.warn(
-          "`render_component` has been deprecated in favor of `render_inline`, and will be removed in v2.0.0."
-        )
-
-        render_inline(component, args, &block)
-      end
-
       def with_variant(variant)
         old_variants = controller.view_context.lookup_context.variants
 
