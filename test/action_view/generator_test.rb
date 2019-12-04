96a3f7fcfb37fb9f739a4516c079ff595186fc5d
Simon Træls Ravn <cs2@cs2.dk>
2019-12-04 21:19:57 +0100

    Bugfix. Generator creates test with render matching new naming convention.

---
+++ test/action_view/generator_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+require_relative File.expand_path("../../../lib/rails/generators/test_unit/component_generator", __FILE__)
+
+class ActionView::GeneratorTest < ::Rails::Generators::TestCase
+  tests TestUnit::Generators::ComponentGenerator
+  destination File.expand_path("../tmp", File.dirname(__FILE__))
+
+  setup do
+    run_generator %w(Dummy data)
+  end
+
+  teardown do
+    File.delete File.expand_path("../../tmp/test/components/dummy_component_test.rb", __FILE__)
+  end
+
+  test "generates component" do
+    assert_file "../tmp/test/components/dummy_component_test.rb" do |content|
+      assert_match(/render_inline\(DummyComponent, attr: "value"\) { "Hello, components!" }.css\("span"\).to_html/, content)
+    end
+  end
+end
