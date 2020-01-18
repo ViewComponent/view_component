ef0025e72c2ef09362329853606d215a71947c6c
asgerb <a@asgerbehnckejacobsen.dk>
2020-01-18 23:11:58 +0100

    Pass requires_content? to the template engine generator

--- lib/rails/generators/component/component_generator.rb
+++ lib/rails/generators/component/component_generator.rb
@@ -6,13 +6,18 @@ module Rails
       source_root File.expand_path("templates", __dir__)
 
       argument :attributes, type: :array, default: [], banner: "attribute"
-      hook_for :template_engine, :test_framework
       check_class_collision suffix: "Component"
 
       class_option :require_content, type: :boolean, default: false
 
       def create_component_file
         template "component.rb", File.join("app/components", class_path, "#{file_name}_component.rb")
+      end
+
+      hook_for :test_framework
+
+      hook_for :template_engine do |instance, template_engine|
+        instance.invoke template_engine, [ instance.name ], require_content: instance.send(:requires_content?)
       end
 
       private
@@ -22,7 +27,10 @@ module Rails
       end
 
       def requires_content?
-        options["require_content"]
+        return @requires_content if @asked
+
+        @asked = true
+        @requires_content = ask("Would you like #{class_name} to require content? (Y/n)").downcase == "y"
       end
 
       def parent_class
