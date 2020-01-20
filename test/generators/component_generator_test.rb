31997c63fde92e625b538033a4e7c253f3c05145
asgerb <a@asgerbehnckejacobsen.dk>
2020-01-20 23:41:06 +0100

    Move test_component_with_required_content up

--- test/generators/component_generator_test.rb
+++ test/generators/component_generator_test.rb
@@ -13,19 +13,6 @@ class ComponentGeneratorTest < Rails::Generators::TestCase
 
   arguments %w[user]
 
-  def test_component_without_required_content
-    without_required_content { run_generator }
-
-    assert_file "app/components/user_component.rb" do |component|
-      assert_match(/class UserComponent < /, component)
-      refute_match(/validates :content, presence: true/, component)
-    end
-
-    assert_file "app/components/user_component.html.erb" do |view|
-      assert_match(/<div>Add User template here<\/div>/, view)
-    end
-  end
-
   def test_component_with_required_content
     with_required_content { run_generator }
 
@@ -36,6 +23,19 @@ class ComponentGeneratorTest < Rails::Generators::TestCase
 
     assert_file "app/components/user_component.html.erb" do |view|
       assert_match(/<%= content %>/, view)
+    end
+  end
+
+  def test_component_without_required_content
+    without_required_content { run_generator }
+
+    assert_file "app/components/user_component.rb" do |component|
+      assert_match(/class UserComponent < /, component)
+      refute_match(/validates :content, presence: true/, component)
+    end
+
+    assert_file "app/components/user_component.html.erb" do |view|
+      assert_match(/<div>Add User template here<\/div>/, view)
     end
   end
 
