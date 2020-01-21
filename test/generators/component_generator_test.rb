df82b0fed958092298cfc67b8a100b84929f62ce
asgerb <a@asgerbehnckejacobsen.dk>
2020-01-21 01:18:55 +0100

    Add template engine tests

--- test/generators/component_generator_test.rb
+++ test/generators/component_generator_test.rb
@@ -37,6 +37,24 @@ class ComponentGeneratorTest < Rails::Generators::TestCase
     assert_file "app/components/admins/user_component.rb", /class Admins::UserComponent < /
   end
 
+  def test_invoking_erb_template_engine
+    without_required_content { run_generator %w[user --template-engine erb] }
+
+    assert_file "app/components/user_component.html.erb"
+  end
+
+  def test_invoking_slim_template_engine
+    without_required_content { run_generator %w[user --template-engine slim] }
+
+    assert_file "app/components/user_component.html.slim"
+  end
+
+  def test_invoking_haml_template_engine
+    without_required_content { run_generator %w[user --template-engine haml] }
+
+    assert_file "app/components/user_component.html.haml"
+  end
+
   private
 
   def with_required_content
