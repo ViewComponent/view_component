b99bbef0dc54def3d9c2b427fa6e23724e031374
asgerb <a@asgerbehnckejacobsen.dk>
2020-01-21 01:18:35 +0100

    Move erb template generation testing into separate test

--- test/generators/component_generator_test.rb
+++ test/generators/component_generator_test.rb
@@ -20,10 +20,6 @@ class ComponentGeneratorTest < Rails::Generators::TestCase
       assert_match(/class UserComponent < /, component)
       assert_match(/validates :content, presence: true/, component)
     end
-
-    assert_file "app/components/user_component.html.erb" do |view|
-      assert_match(/<%= content %>/, view)
-    end
   end
 
   def test_component_without_required_content
@@ -33,17 +29,12 @@ class ComponentGeneratorTest < Rails::Generators::TestCase
       assert_match(/class UserComponent < /, component)
       refute_match(/validates :content, presence: true/, component)
     end
-
-    assert_file "app/components/user_component.html.erb" do |view|
-      assert_match(/<div>Add User template here<\/div>/, view)
-    end
   end
 
   def test_component_with_namespace
     with_required_content { run_generator %w[admins/user] }
 
     assert_file "app/components/admins/user_component.rb", /class Admins::UserComponent < /
-    assert_file "app/components/admins/user_component.html.erb"
   end
 
   private
