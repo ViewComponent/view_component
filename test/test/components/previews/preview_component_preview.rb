c55ec41b9ae7aacfb4a8f3799d0cce9aff6c74f0
Joel Hawksley <joel@hawksley.org>
2020-02-14 15:16:54 -0700

    update test suite to use MyComponent.new syntax

--- test/test/components/previews/preview_component_preview.rb
+++ test/test/components/previews/preview_component_preview.rb
@@ -2,14 +2,14 @@
 
 class PreviewComponentPreview < ActionView::Component::Preview
   def default
-    render(PreviewComponent, cta: "Click me!", title: "Lorem Ipsum")
+    render(PreviewComponent.new(cta: "Click me!", title: "Lorem Ipsum"))
   end
 
   def without_cta
-    render(PreviewComponent, title: "More lorem...")
+    render(PreviewComponent.new(title: "More lorem..."))
   end
 
   def with_content
-    render(PreviewComponent, title: "title") { "some content" }
+    render(PreviewComponent.new(title: "title")) { "some content" }
   end
 end
