e42cd138e61d41548feadbf34dc4e8f9d7033de1
Joel Hawksley <joel@hawksley.org>
2020-02-26 07:39:26 -0700

    release v1.12.0

--- lib/action_view/component/version.rb
+++ lib/action_view/component/version.rb
@@ -4,8 +4,8 @@ module ActionView
   module Component
     module VERSION
       MAJOR = 1
-      MINOR = 11
-      PATCH = 1
+      MINOR = 12
+      PATCH = 0
 
       STRING = [MAJOR, MINOR, PATCH].join(".")
     end
