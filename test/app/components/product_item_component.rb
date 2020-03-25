85806b4b67183342e82bcccab76b7c613c5d9648
Timothy Clem <timothy.clem@gmail.com>
2020-03-25 13:44:44 -0700

    Duplicative of ProductCoupon

--- test/app/components/product_item_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ProductItemComponent < ViewComponent::Base
-  def initialize(product_item:, extra:)
-    @product_item = product_item
-    @extra = extra
-  end
-end
