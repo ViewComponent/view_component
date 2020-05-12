269ae7f031499dde8cfb794eb7b001a57516ced0
Joel Hawksley <joel@hawksley.org>
2020-05-12 11:11:16 -0600

    add annotations for caching and CSRF tokens

--- lib/view_component/base.rb
+++ lib/view_component/base.rb
@@ -10,6 +10,7 @@ module ViewComponent
     include ActiveSupport::Configurable
     include ViewComponent::Previewable
 
+    # For CSRF authenticity tokens in forms
     delegate :form_authenticity_token, :protect_against_forgery?, to: :helpers
 
     class_attribute :content_areas, default: []
@@ -58,6 +59,7 @@ module ViewComponent
       # For template variants (+phone, +desktop, etc.)
       @variant = @lookup_context.variants.first
 
+      # For caching, such as #cache_if
       old_current_template = @current_template
       @current_template = self
 
