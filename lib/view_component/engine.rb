ViewComponent/view_component

commit e45338fdb26044f43e8c576da5fcb21a2aa4113e

add support for multiple preview paths

lib/view_component/engine.rb
@@ -6,6 +6,7 @@ require "view_component"
 module ViewComponent
   class Engine < Rails::Engine # :nodoc:
     config.view_component = ActiveSupport::OrderedOptions.new
+    config.view_component.preview_paths ||= []
 
     initializer "view_component.set_configs" do |app|
       options = app.config.view_component
@@ -14,7 +15,7 @@ module ViewComponent
       options.preview_route ||= ViewComponent::Base.preview_route
 
       if options.show_previews
-        options.preview_path ||= defined?(Rails.root) ? "#{Rails.root}/test/components/previews" : nil
+        options.preview_paths << "#{Rails.root}/test/components/previews" if defined?(Rails.root)
       end
 
       ActiveSupport.on_load(:view_component) do

lib/view_component/preview.rb
@@ -65,13 +65,13 @@ module ViewComponent # :nodoc:
       private
 
       def load_previews
-        if preview_path
+        Array(preview_paths).each do |preview_path|
           Dir["#{preview_path}/**/*_preview.rb"].sort.each { |file| require_dependency file }
         end
       end
 
-      def preview_path
-        Base.preview_path
+      def preview_paths
+        Base.preview_paths
       end
 
       def show_previews

lib/view_component/previewable.rb
@@ -9,9 +9,9 @@ module ViewComponent # :nodoc:
     included do
       # Set the location of component previews through app configuration:
       #
-      #     config.view_component.preview_path = "#{Rails.root}/lib/component_previews"
+      #     config.view_component.preview_paths << "#{Rails.root}/lib/component_previews"
       #
-      mattr_accessor :preview_path, instance_writer: false
+      mattr_accessor :preview_paths, instance_writer: false
 
       # Enable or disable component previews through app configuration:
       #

test/config/environments/test.rb
@@ -20,7 +20,7 @@ Dummy::Application.configure do
   config.action_controller.allow_forgery_protection    = false
 
   config.view_component.show_previews = true
-
+  config.view_component.preview_paths << "#{Rails.root}/lib/component_previews"
   config.view_component.test_controller = "IntegrationExamplesController"
 
   # Tell Action Mailer not to deliver emails to the real world.

test/lib/component_previews/my_component_lib_preview.rb
@@ -1,0 +1,9 @@
+# frozen_string_literal: true
+
+class MyComponentLibPreview < ViewComponent::Preview
+  layout false
+
+  def default
+    render(MyComponent.new)
+  end
+end

test/view_component/integration_test.rb
@@ -312,6 +312,11 @@ class IntegrationTest < ActionDispatch::IntegrationTest
     assert_select "title", "Component Previews for preview_component"
   end
 
+  test "test preview from multiple preview_paths" do
+    get "/rails/view_components/my_component_lib/default"
+    assert_select("div", "hello,world!")
+  end
+
   test "renders collections" do
     get "/products"
 

commit ac2cb1eb3acfd32aa47d7f1378c7c0e635ae8287

Fix auto-loading of previews
Currently, the Rails server has to be manually restarted in order to
pick up changes to view_component preview files. This is a bug. Auto-
loading for previews worked before, but I believe it was inadvertently
broken in e45338f.

This PR restores the auto-loading behavior for previews. Now if you
make a change to a preview `.rb` file, the changes take effect without
a server restart.

lib/view_component/engine.rb
@@ -34,8 +34,8 @@ module ViewComponent
     initializer "view_component.set_autoload_paths" do |app|
       options = app.config.view_component
 
-      if options.show_previews && options.preview_path
-        ActiveSupport::Dependencies.autoload_paths << options.preview_path
+      if options.show_previews && !options.preview_paths.empty?
+        ActiveSupport::Dependencies.autoload_paths.concat(options.preview_paths)
       end
     end
 