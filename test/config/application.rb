eaee7d02dfd64d8ee4f10ca7af3299495bce44b1
Mitchell Gould <mitchellgould7@gmail.com>
2020-08-21 14:59:25 -0400

    add action_text/engine module required for tests

--- test/config/application.rb
+++ test/config/application.rb
@@ -5,6 +5,7 @@ require File.expand_path("../boot", __FILE__)
 require "active_model/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
+require "action_text/engine"
 require "view_component/engine"
 require "sprockets/railtie"
 
