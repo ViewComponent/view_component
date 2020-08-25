9b4cb4302122d3ec19921b3f5f86343558304255
Joel Hawksley <joel@hawksley.org>
2020-08-25 11:05:15 -0600

    did I need to add that line?

--- test/app/controllers/integration_examples_controller.rb
+++ test/app/controllers/integration_examples_controller.rb
@@ -16,9 +16,6 @@ class IntegrationExamplesController < ActionController::Base
   end
 
   def controller_to_string
-    # ensure render_to_string isn't broken by monkey patch
-    render_to_string("integration_examples/_controller_inline", locals: { message: "bar" })
-
     render(plain: render_to_string(ControllerInlineComponent.new(message: "bar")))
   end
 
