051642e62b245dc988fb708906538f6a4e90d769
Manuel Puyol <manuelpuyol@github.com>
2021-03-08 12:39:31 -0600

    update ContainerComponent

--- test/view_component/view_component_test.rb
+++ test/view_component/view_component_test.rb
@@ -254,7 +254,7 @@ class ViewComponentTest < ViewComponent::TestCase
   end
 
   def test_renders_helper_method_within_nested_component
-    render_inline(HelpersContainerComponent.new)
+    render_inline(ContainerComponent.new)
 
     assert_text("Hello helper method")
   end
