e777a971f75ac7f5876b66cd8b25534056f38377
Elia Schito <elia@schito.me>
2021-03-16 22:47:06 +0100

    Do not silence library backtraces in test reports

--- test/config/application.rb
+++ test/config/application.rb
@@ -19,3 +19,7 @@ module Dummy
 end
 
 Dummy::Application.config.secret_key_base = "foo"
+
+# Do not silence library backtraces in test reports
+Rails.backtrace_cleaner.remove_filters!
+Rails.backtrace_cleaner.remove_silencers!
