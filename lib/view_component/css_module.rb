f6d67e24ff5f39f04e6656144457e549edb542f2
Joel Hawksley <joel@hawksley.org>
2021-03-24 13:03:11 -0600

    add Ruby implementation of CSS Modules
    Co-authored-by: Robert Mosolgo <rmosolgo@github.com>

---
+++ lib/view_component/css_module.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+
+require "sass"
+
+module ViewComponent
+  # Take CSS module code as input, and rewrite it as
+  # browser-friendly CSS code. Apply opaque transformations
+  # so that selectors can only be accessed programatically,
+  # not by class name literals.
+  class CSSModule < Sass::Tree::Visitors::Base
+    def initialize(module_name, raw_css)
+      @module_name, @mappings = module_name, {}
+
+      # Parse incoming CSS into an AST
+      @css_root = Sass::SCSS::CssParser.new(raw_css, "(CSSModules)", 1).parse
+
+      @module_hash = compute_hash
+    end
+
+    def rewrite
+      Sass::Tree::Visitors::SetOptions.visit(@css_root, {})
+
+      {
+        mappings: mappings,
+        css: @css_root.render
+      }
+    end
+
+    private
+
+    # Generate a short, random-ish token to prevent CSS selector collisions.
+    def compute_hash
+      Digest::MD5.hexdigest(@module_name).first(5) # rubocop:disable GitHub/InsecureHashAlgorithm
+    end
+
+    def visit_rule(node)
+      node.parsed_rules = rebuild_parsed_rules(node.parsed_rules)
+    end
+
+    def mappings
+      visit(@css_root)
+
+      @mappings
+    end
+
+    def rebuild_parsed_rules(parsed_rules)
+      new_members = parsed_rules.members.map do |member_seq|
+        deeply_transform(member_seq)
+      end
+      Sass::Selector::CommaSequence.new(new_members)
+    end
+
+    # Get the first Sass::Selector::Pseudo member of `seq`, or nil
+    def first_pseudo_selector(seq)
+      case seq
+      when Sass::Selector::AbstractSequence
+        seq.members.find { |m| first_pseudo_selector(m) }
+      when Sass::Selector::Pseudo
+        seq
+      else # eg, String
+        nil
+      end
+    end
+
+    # Combine `module_name` and `selector`, but don't prepend a `.` or `#`
+    # because this value will be inserted into the HTML page as `class=` or `id=`
+    # @param module_name [String] A CSS module name
+    # @param selector [String] A would-be DOM selector (without the leading `.` or `#`)
+    # @return [String] An opaque selector for this module-selector pair
+    def modulize_selector(selector)
+      after = "#{@module_name}_#{@module_hash}_#{selector}"
+
+      @mappings[selector] = after
+
+      after
+    end
+
+    # We know this is a modulized rule
+    # now we should transform its ID and classes to modulized
+    def deeply_transform(seq)
+      case seq
+      when Sass::Selector::AbstractSequence
+        new_members = seq.members.map { |m| deeply_transform(m) }
+        new_members.compact! # maybe a module selector returned nil
+        clone_sequence(seq, new_members)
+      when Sass::Selector::Id, Sass::Selector::Class
+        # Sass 3.2 has an array here, Sass 3.4 has a string:
+        selector_name = seq.name.is_a?(Array) ? seq.name.first : seq.name
+        modulized_name = modulize_selector(selector_name)
+        seq.class.new(modulized_name)
+      when Sass::Selector::Pseudo
+        if seq.to_s =~ /:module/
+          nil
+        else
+          seq
+        end
+      else
+        seq
+      end
+    end
+
+    # Make a new kind of `seq`, containing `new_members`
+    def clone_sequence(seq, new_members)
+      case seq
+      when Sass::Selector::Sequence
+        seq.class.new(new_members)
+      when Sass::Selector::CommaSequence
+        seq.class.new(new_members)
+      when Sass::Selector::SimpleSequence
+        seq.class.new(new_members, seq.subject?)
+      else
+        raise("Unknown sequence to clone: #{seq.class}")
+      end
+    end
+
+    # Debug print-out of a sequence:
+    def deep_print(seq, indent = "")
+      case seq
+      when Sass::Selector::AbstractSequence
+        puts indent + seq.class.name + " => "
+        seq.members.map { |m| deep_print(m, indent + "  ") }
+      else
+        puts indent + seq.class.name + " (#{seq})"
+      end
+    end
+  end
+end
