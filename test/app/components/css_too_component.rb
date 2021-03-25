d894c2b397b20f7f8830056db29b26b9abf7c9b5
Joel Hawksley <joel@hawksley.org>
2021-03-25 12:45:36 -0600

    remove unused component

--- test/app/components/css_too_component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CssTooComponent < ViewComponent::Base
-  include ViewComponent::Stylable
-
-  def call
-    content_tag(:div, "Hello, World!", class: styles['foo'])
-  end
-end
