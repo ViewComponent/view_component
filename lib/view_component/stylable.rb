6e635a89ba3380635ffd2270217da7cedaee21b0
Joel Hawksley <joel@hawksley.org>
2021-03-25 10:21:46 -0600

    flesh out internal code comments

--- lib/view_component/stylable.rb
+++ lib/view_component/stylable.rb
@@ -4,12 +4,19 @@ require "active_support/concern"
 require "view_component/css_module"
 
 module ViewComponent
-  # EXPERIMENTAL
-  # Support for CSS Modules. Use at your own peril!
+  # EXPERIMENTAL - This module is subject to change
+  # at any time without warning. Use at your own peril!
+  #
+  # Adds support for sidecar CSS modules.
+  # See docs/index.md for directions for use.
   module Stylable
     extend ActiveSupport::Concern
 
     class_methods do
+      # If a sidecar CSS file exists, we compile it onto the
+      # component class, storing the resulting CSS and
+      # mappings from the original selector names to their
+      # hashed equivalents.
       def _after_compile
         return unless css_file_path = _sidecar_files(["css"]).first
 
@@ -22,12 +29,21 @@ module ViewComponent
         @styles = rewrite[:mappings]
       end
 
-      def css
+      # The compiled CSS.
+      #
+      # For example:
+      #   ".Css_0343d_foo { color: red; }"
+      def _css
         ensure_compiled
 
         @css
       end
 
+      # Hash of mappings from original selector name to
+      # the hashed equivalent.
+      #
+      # For example:
+      #   { "foo" => "Css_0343d_foo"}
       def styles
         ensure_compiled
 
@@ -35,22 +51,29 @@ module ViewComponent
       end
     end
 
+    # After component is rendered, attach the style tag
+    # that contains the compiled CSS.
     def _after_render
-      style_tag
+      _style_tag
     end
 
-    def style_tag
+    # Style tag with compiled CSS.
+    #
+    # Registers component with current request, preventing
+    # the rendering of the style tag more than once for the
+    # same component in a single request.
+    def _style_tag
       rendered_components = request.params["___rendered_view_components"] ||= Set.new
       rendered_components_identifier = "#{Rails.application.class.name}::#{self.class.name}"
 
-      # Only render the component's style tag once per request
       if !rendered_components.include?(rendered_components_identifier)
         request.params["___rendered_view_components"] << rendered_components_identifier
 
-        content_tag(:style, self.class.css.html_safe) # rubocop:disable Rails/OutputSafety
+        content_tag(:style, self.class._css.html_safe) # rubocop:disable Rails/OutputSafety
       end
     end
 
+    # Instance convenience accessor for .styles
     def styles
       self.class.styles
     end
