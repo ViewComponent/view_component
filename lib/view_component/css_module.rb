6aabcd007145540284cc1260edbdfc3e3d97a692
Joel Hawksley <joel@hawksley.org>
2021-03-26 16:07:42 -0600

    remove unused code

--- lib/view_component/css_module.rb
+++ lib/view_component/css_module.rb
@@ -53,18 +53,6 @@ module ViewComponent
       Sass::Selector::CommaSequence.new(new_members)
     end
 
-    # Get the first Sass::Selector::Pseudo member of `seq`, or nil
-    def first_pseudo_selector(seq)
-      case seq
-      when Sass::Selector::AbstractSequence
-        seq.members.find { |m| first_pseudo_selector(m) }
-      when Sass::Selector::Pseudo
-        seq
-      else # eg, String
-        nil
-      end
-    end
-
     # Combine `module_name` and `selector`, but don't prepend a `.` or `#`
     # because this value will be inserted into the HTML page as `class=` or `id=`
     # @param module_name [String] A CSS module name
@@ -107,12 +95,8 @@ module ViewComponent
       case seq
       when Sass::Selector::Sequence
         seq.class.new(new_members)
-      when Sass::Selector::CommaSequence
-        seq.class.new(new_members)
       when Sass::Selector::SimpleSequence
         seq.class.new(new_members, seq.subject?)
-      else
-        raise("Unknown sequence to clone: #{seq.class}")
       end
     end
   end
