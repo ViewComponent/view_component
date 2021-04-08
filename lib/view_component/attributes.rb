b30596a9f7f817c310cf828aafe185695d5523c4
Blake Williams <blake@blakewilliams.me>
2021-04-08 15:21:02 -0400

    Add basic attributes API

---
+++ lib/view_component/attributes.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+# An attributes API for declaratively defining properties a component accepts.
+# @example
+#   class PostComponent
+#     include ViewComponent::Attributes
+#
+#     accepts :title, required: true
+#     accepts :posted_at, required: true
+#
+#     accepts :author, default: NullUser.new
+#   end
+#
+#   <%= render PostComponent.new(title: "foo", posted_at: Date.yesterday %>
+module ViewComponent
+  module Attributes
+    extend ActiveSupport::Concern
+
+    included do
+      cattr_accessor :_optional_attributes, default: {}
+      cattr_accessor :_required_attributes, default: {}
+    end
+
+    class_methods do
+      def accepts(parameter, required: false, default: nil)
+        if required
+          _required_attributes[parameter] = default
+        else
+          _optional_attributes[parameter] = default
+        end
+
+        attr_accessor parameter
+      end
+    end
+
+    def initialize(**args)
+      construct_params(args)
+    end
+
+    private
+
+    def construct_params(args)
+      _required_attributes.each do |attr, _default|
+        if !args.has_key?(attr)
+          raise ArgumentError.new("Missing keyword: #{attr}") # Simulate required kwargs
+        end
+
+        instance_variable_set("@#{attr}", args[attr])
+      end
+
+      _optional_attributes.each do |attr, default|
+        if args.has_key?(attr)
+          value = args[attr]
+          instance_variable_set("@#{attr}", value)
+        else
+          value = args[attr]
+          instance_variable_set("@#{attr}", default)
+        end
+      end
+    end
+  end
+end
---
+++ test/view_component/attributes_test.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "view_component/attributes"
+
+class AttributesTest < ViewComponent::TestCase
+  class MyAttributeComponent < ViewComponent::Base
+    include ViewComponent::Attributes
+
+    accepts :title, required: true
+    accepts :body
+    accepts :posted_at, default: Date.today
+
+    def call
+      content_tag :div do
+        safe_join([
+          content_tag(:h1, @title),
+          content_tag(:p, @body),
+          content_tag(:date, @posted_at),
+        ])
+      end
+    end
+  end
+
+  def test_basic_attributes
+    render_inline MyAttributeComponent.new(title: "foo")
+  end
+
+  def test_required_attribute_raises_if_missing
+    assert_raises ArgumentError do
+      render_inline MyAttributeComponent.new
+    end
+  end
+
+  def test_optional_arguments_default_to_nil
+    component = MyAttributeComponent.new(title: "foo")
+    assert_nil component.body
+  end
+
+  def test_default_values_are_set_if_argument_is_missing
+    freeze_time do
+      component = MyAttributeComponent.new(title: "foo")
+      assert_equal Date.today, component.posted_at
+    end
+  end
+
+  def test_explicit_nil_overrides_default_values
+    component = MyAttributeComponent.new(title: "foo", posted_at: nil)
+    assert_nil component.posted_at
+  end
+end
