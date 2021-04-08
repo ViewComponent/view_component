8872699a3cdb4c02f365a729b66dc42b4361e3a1
Elia Schito <elia@schito.me>
2021-04-08 22:58:01 +0200

    Fix a typo in the translatable benchmark

--- performance/translatable_benchmark.rb
+++ performance/translatable_benchmark.rb
@@ -10,7 +10,7 @@ ENV["RAILS_ENV"] = "production"
 require File.expand_path("../test/config/environment.rb", __dir__)
 
 require_relative "components/global_i18n_component.rb"
-require_relative "components/sidecar_i18n_component.rb"
+require_relative "components/translatable_component.rb"
 
 class BenchmarksController < ActionController::Base
 end
@@ -23,7 +23,7 @@ I18n.backend.load_translations
 Benchmark.ips do |x|
   x.report(:global) { controller_view.render(GlobalI18nComponent.new("hello")) }
   x.report(:global_missing) { controller_view.render(GlobalI18nComponent.new("missing")) }
-  x.report(:sidecar_absolute) { controller_view.render(TranslatableComponent.new("sidecar_i18n_component.hello")) }
+  x.report(:sidecar_absolute) { controller_view.render(TranslatableComponent.new("translatable_component.hello")) }
   x.report(:sidecar) { controller_view.render(TranslatableComponent.new(".hello")) }
   x.report(:sidecar_missing) { controller_view.render(TranslatableComponent.new("missing")) }
   x.report(:sidecar_fallback) { controller_view.render(TranslatableComponent.new("from.rails")) }
