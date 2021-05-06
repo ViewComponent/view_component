35c3949c13b66d865288806357e3ba9758fdb99a
Simon Fish <si@mon.fish>
2021-05-06 16:15:01 +0100

    Add test for rendering component from template without newline

--- test/view_component/integration_test.rb
+++ test/view_component/integration_test.rb
@@ -443,6 +443,11 @@ class IntegrationTest < ActionDispatch::IntegrationTest
     end
   end
 
+  def test_does_not_render_additional_newline
+    get "/rails/view_components/display_inline_component/with_newline"
+    assert_includes response.body, "<span>Hello, world!</span><span>Hello, world!</span>"
+  end
+
   def test_renders_the_preview_example_with_its_own_template_and_a_layout
     get "/rails/view_components/my_component/inside_banner"
     assert_includes response.body, "ViewComponent - Admin - Test"
