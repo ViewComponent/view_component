196cb0bffbc14db762c8f2c097c608cb9f9a0fe2
Edouard Piron <ed.piron@gmail.com>
2021-05-22 00:11:33 +0200

    Ensure components are loaded

--- app/helpers/view_component_helper.rb
+++ app/helpers/view_component_helper.rb
@@ -1,6 +1,7 @@
 module ViewComponentHelper
   # For each component, define an helper that renders the component
   # Forward arguments, keywords and block
+  Dir[Rails.root.join("app/components/*.rb")].each { |file| require file }
   ViewComponent::Base.subclasses.each do |component|
     method_name = component.name.underscore.tr('/', '_')
     define_method(method_name) do |*args, &block|
