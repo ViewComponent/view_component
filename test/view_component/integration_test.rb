882c3f38ee57bce3d67a27827cb1238480956e9d
Simon Fish <si@mon.fish>
2021-06-15 13:36:41 +0100

    Update tests for trailing newlines (#958)
    Update tests with trailing newline solution
    
    The tests introduced in #920 were made to pass by removing the trailing newline. These were supposed to check that trailing newlines in template files would not become part of the output. Now that the source issue has reached a conclusion thanks to an update to ActionView::Template::Handlers::ERB, the implementation for the test can be updated to reflect this. It also makes clear that we're dependent on that class for this change.

--- test/view_component/integration_test.rb
+++ test/view_component/integration_test.rb
@@ -437,10 +437,14 @@ class IntegrationTest < ActionDispatch::IntegrationTest
   end
 
   def test_does_not_render_additional_newline
+    skip unless Rails::VERSION::MAJOR >= 7
     without_template_annotations do
+      ActionView::Template::Handlers::ERB.strip_trailing_newlines = true
       get "/rails/view_components/display_inline_component/with_newline"
       assert_includes response.body, "<span>Hello, world!</span><span>Hello, world!</span>"
     end
+  ensure
+    ActionView::Template::Handlers::ERB.strip_trailing_newlines = false if Rails::VERSION::MAJOR >= 7
   end
 
   def test_renders_the_preview_example_with_its_own_template_and_a_layout
