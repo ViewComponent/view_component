565e77379029e0721f7ccf6c96ce330af4cccfdf
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-06-24 14:20:59 +0100

    Add custom dependency tracker for components

---
+++ lib/view_component/caching/component_tracker.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module ViewComponent
+  module Caching
+    class ComponentTracker < ERBTracker
+      def initialize(name, template, view_paths = nil)
+        super
+        @component_class = name.safe_constantize
+      end
+
+      def dependencies
+        return [] unless view_component?
+
+        super + templates + relevant_ancestors
+      end
+
+      private
+
+      attr_reader :component_class
+
+      def view_component?
+        component_class&.ancestors&.include? ViewComponent::Base
+      end
+
+      # Returns relevant templates for the component
+      def templates
+        component_class.
+          _sidecar_files(ActionView::Template.template_handler_extensions).
+          map { |path| path.gsub(%r{(.*app/components/)|(\..*)}, "") }
+      end
+
+      # Returns relevant ancestors the component inherits from (if any)
+      def relevant_ancestors
+        ((component_class.ancestors & ViewComponent::Base.descendants) - [component_class]).
+          map(&:to_s)
+      end
+    end
+  end
+end
