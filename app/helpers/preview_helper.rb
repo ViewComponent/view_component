d897d571790da35c5a825597f9a0689d0cdd08b8
Edwin <edwinthinks@gmail.com>
2021-08-24 20:44:43 -0500

    Utilize template file look-up to address rails 5.x issue

--- app/helpers/preview_helper.rb
+++ app/helpers/preview_helper.rb
@@ -5,7 +5,13 @@ module PreviewHelper
   FALLBACK_LANGUAGE = "ruby"
 
   def prism_language_name(template:)
-    language = template.identifier.split(".").last
+    if template.respond_to?(:identifier)
+      language = template.identifier.split(".").last
+    else
+      # Handle case when the template is a path
+      language = template.gsub('.html', '').split('.').last
+    end
+
     return FALLBACK_LANGUAGE unless AVAILABLE_PRISM_LANGUAGES.include? language
 
     language
@@ -16,4 +22,29 @@ module PreviewHelper
 
     render "preview_source" # rubocop:disable GitHub/RailsViewRenderPathsExist
   end
+
+  def find_template_source(lookup_context:, template_identifier:)
+    template = lookup_context.find_template(template_identifier)
+    return template if Rails.version.to_f >= 6.1 || template.source.present?
+
+    # Fetch template source via finding it through preview paths
+    # to accomodate source view when exclusively using templates
+    # for previews for Rails < 6.1.
+    all_template_paths = ViewComponent::Base.preview_paths.map do |preview_path|
+      Dir.glob("#{preview_path}/**/*")
+    end.flatten
+
+    # Search for templates the contain `html`.
+    matching_templates = all_template_paths.find_all do |template|
+      template =~ /#{template_identifier}*.(html)/
+    end
+
+    # In-case of a conflict due to multiple template files with
+    # the same name
+    raise "found 0 matches for templates for #{template_identifier}." if matching_templates.empty?
+    raise "found multiple templates for #{template_identifier}." if matching_templates.size > 1
+
+    File.read(matching_templates.first)
+  end
+
 end
--- test/view_component/preview_helper_test.rb
+++ test/view_component/preview_helper_test.rb
@@ -19,4 +19,99 @@ class PreviewHelperTest < ActiveSupport::TestCase
     assert_equal(PreviewHelper.prism_language_name(template: template), "ruby")
     template.verify
   end
+
+  def test_returns_language_using_a_template_path
+    template_path = 'test.html.erb'
+    assert_equal(PreviewHelper.prism_language_name(template: template_path), "erb")
+  end
+
+  def test_returns_language_using_a_template_path_with_haml
+    template_path = 'test.haml.html'
+    assert_equal(PreviewHelper.prism_language_name(template: template_path), "haml")
+  end
+
+  def test_returns_language_using_a_template_with_fallback
+    template_path = 'test.slim.html'
+    assert_equal(PreviewHelper.prism_language_name(template: template_path), "ruby")
+  end
+
+  def test_returns_the_template_source
+    template_identifier = "preview/no_template"
+
+    expected_template_source = "expected_template"
+    mock_template = Minitest::Mock.new
+    mock_template.expect(:source, expected_template_source)
+    mock_template.expect(:source, expected_template_source)
+
+    lookup_context = Minitest::Mock.new
+    lookup_context.expect(:find_template, mock_template, [template_identifier])
+
+    template = PreviewHelper.find_template_source(lookup_context: lookup_context, template_identifier: template_identifier)
+    assert_equal(template.source, "expected_template")
+  end
+
+  if Rails.version.to_f < 6.1
+    def test_returns_the_template_source_with_template
+      template_identifier = "preview/template"
+      expected_source = "<%= PreviewTest %>"
+
+      mock_template = Minitest::Mock.new
+      mock_template.expect(:source, "")
+      mock_template.expect(:source, "")
+
+      lookup_context = Minitest::Mock.new
+      lookup_context.expect(:find_template, mock_template, [template_identifier])
+
+      mock = Minitest::Mock.new
+      mock.expect :map, [template_identifier + ".html.haml"]
+      ViewComponent::Base.stub :preview_paths, mock do
+        File.stub(:read, expected_source) do
+          template = PreviewHelper.find_template_source(lookup_context: lookup_context, template_identifier: template_identifier)
+          assert_equal(template, expected_source)
+        end
+      end
+    end
+
+    def test_returns_the_template_source_with_template_but_no_matching_template_found
+      template_identifier = "preview/template"
+
+      mock_template = Minitest::Mock.new
+      mock_template.expect(:source, "")
+      mock_template.expect(:source, "")
+
+      lookup_context = Minitest::Mock.new
+      lookup_context.expect(:find_template, mock_template, [template_identifier])
+
+      mock = Minitest::Mock.new
+      mock.expect :map, []
+      ViewComponent::Base.stub :preview_paths, mock do
+        exception = assert_raises RuntimeError do
+          PreviewHelper.find_template_source(lookup_context: lookup_context, template_identifier: template_identifier)
+        end
+
+        assert_equal("found 0 matches for templates for #{template_identifier}.", exception.message)
+      end
+    end
+
+    def test_returns_the_template_source_with_template_but_there_is_a_conflict
+      template_identifier = "preview/template"
+
+      mock_template = Minitest::Mock.new
+      mock_template.expect(:source, "")
+      mock_template.expect(:source, "")
+
+      lookup_context = Minitest::Mock.new
+      lookup_context.expect(:find_template, mock_template, [template_identifier])
+
+      mock = Minitest::Mock.new
+      mock.expect :map, [template_identifier + ".html.haml", template_identifier + ".html.erb"]
+      ViewComponent::Base.stub :preview_paths, mock do
+        exception = assert_raises RuntimeError do
+          PreviewHelper.find_template_source(lookup_context: lookup_context, template_identifier: template_identifier)
+        end
+        assert_equal("found multiple templates for #{template_identifier}.", exception.message)
+      end
+    end
+  end
+
 end
