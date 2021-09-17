e48276043fcd865f5f24e53a5c57ad3caf09fb0e
Hans Lemuet <hans@spone.net>
2021-09-17 16:10:39 +0200

    Make rubocop happy

--- lib/view_component/base.rb
+++ lib/view_component/base.rb
@@ -286,7 +286,9 @@ module ViewComponent
     #     config.view_component.component_parent_class = "MyBaseComponent"
     #
     # Defaults to "ApplicationComponent" if defined, "ViewComponent::Base" otherwise.
-    mattr_accessor :component_parent_class, instance_writer: false, default: defined?(ApplicationComponent) ? "ApplicationComponent" : "ViewComponent::Base"
+    mattr_accessor :component_parent_class,
+                   instance_writer: false,
+                   default: defined?(ApplicationComponent) ? "ApplicationComponent" : "ViewComponent::Base"
 
     class << self
       # @private
