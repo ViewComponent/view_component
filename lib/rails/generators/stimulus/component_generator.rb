0869185868a8caa912eda132744c37749d3366a5
Peter Sumskas <petersumskas@gmail.com>
2021-11-01 21:54:13 +1100

    Add require to load AbstractGenerator

--- lib/rails/generators/stimulus/component_generator.rb
+++ lib/rails/generators/stimulus/component_generator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "rails/generators/erb/component_generator"
+
 module Stimulus
   module Generators
     class ComponentGenerator < ::Rails::Generators::NamedBase
