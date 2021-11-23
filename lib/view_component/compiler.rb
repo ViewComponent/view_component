f106123fe7b6cb8672ff95ebc399ee6f51a6ca12
Horia Radu <horia.radu@e-spres-oh.com>
2021-11-23 16:17:24 +0200

    Fix typo

--- lib/view_component/compiler.rb
+++ lib/view_component/compiler.rb
@@ -40,7 +40,7 @@ module ViewComponent
         component_class.validate_collection_parameter!
       end
 
-      component_class.lock.synchronize do
+      component_class.mutex.synchronize do
         templates.each do |template|
           # Remove existing compiled template methods,
           # as Ruby warns when redefining a method.
