8e058791513e61ec244d4be11bffa4ad653f28d9
Bob Maerten <bob@levups.com>
2021-12-07 12:58:14 +0100

    Allow to pass optional --translations switch Component generator

--- lib/rails/generators/component/component_generator.rb
+++ lib/rails/generators/component/component_generator.rb
@@ -15,6 +15,7 @@ module Rails
       class_option :parent, type: :string, desc: "The parent class for the generated component"
       class_option :stimulus, type: :boolean, default: ViewComponent::Base.generate_stimulus_controller
       class_option :sidecar, type: :boolean, default: false
+      class_option :translations, type: :boolean, default: false
 
       def create_component_file
         template "component.rb", File.join(component_path, class_path, "#{file_name}_component.rb")
@@ -25,6 +26,8 @@ module Rails
       hook_for :preview, type: :boolean
 
       hook_for :stimulus, type: :boolean
+
+      hook_for :translations, type: :boolean
 
       hook_for :template_engine do |instance, template_engine|
         instance.invoke template_engine, [instance.name]
