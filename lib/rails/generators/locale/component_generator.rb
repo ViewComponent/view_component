82979db7cae5b8e1036b19759c0153623a4e510e
Bob Maerten <bob@levups.com>
2021-12-13 15:52:14 +0100

    Make default behavior more logical and enable tuning in config

---
+++ lib/rails/generators/locale/component_generator.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "rails/generators/abstract_generator"
+
+module Locale
+  module Generators
+    class ComponentGenerator < ::Rails::Generators::NamedBase
+      include ViewComponent::AbstractGenerator
+
+      source_root File.expand_path("templates", __dir__)
+      class_option :sidecar, type: :boolean, default: false
+
+      def create_locale_file
+        if ViewComponent::Base.generate_splitted_locale_files
+          I18n.available_locales.each do |locale|
+            @locales = [locale]
+            template "component.yml", destination(locale)
+          end
+        else
+          @locales = I18n.available_locales.presence || [:en]
+          template "component.yml", destination
+        end
+      end
+
+      private
+
+      def destination(locale = nil)
+        extention = ".#{locale}" if locale
+        if options["sidecar"]
+          File.join(component_path, class_path, "#{file_name}_component", "#{file_name}_component#{extention}.yml")
+        else
+          File.join(component_path, class_path, "#{file_name}_component#{extention}.yml")
+        end
+      end
+    end
+  end
+end
