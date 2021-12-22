66b234f0f05a6a3440432b281770b6f32f22ca39
Gleydson <tavares.gleydson@gmail.com>
2021-12-22 19:59:56 -0300

    add --sidecar flag to the component generator

--- lib/view_component/base.rb
+++ lib/view_component/base.rb
@@ -310,8 +310,15 @@ module ViewComponent
     #     config.view_component.component_parent_class = "MyBaseComponent"
     #
     # Defaults to "ApplicationComponent" if defined, "ViewComponent::Base" otherwise.
-    mattr_accessor :component_parent_class,
-                   instance_writer: false
+    mattr_accessor :component_parent_class, instance_writer: false
+
+    # Always generate a component with a sidecar directory:
+    #
+    #     config.view_component.sidecar = true
+    #
+    # Defaults to `false`.
+    #
+    mattr_accessor :sidecar, instance_writer: false, default: false
 
     class << self
       # @private
