ed981e85c9cd49ed5a79e355d8e094d1f456a4be
Edwin <edwinthinks@gmail.com>
2021-12-29 16:01:55 -0500

    Create tmp folder if it doesn't exist

--- lib/view_component/system_test_helpers.rb
+++ lib/view_component/system_test_helpers.rb
@@ -24,6 +24,9 @@ module ViewComponent
       }
       html = controller.render_to_string("view_components/preview", opts)
 
+      # Add 'tmp/' directory if it doesn't exist to store the rendered component html
+      Dir.mkdir('tmp/') unless Dir.exist?('tmp/')
+
       # Write to temporary file to contain fully rendered component
       # within a browser
       file = Tempfile.new(["rendered_#{component.class.name}", ".html"], "tmp/")
