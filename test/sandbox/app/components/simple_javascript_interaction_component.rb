8e1b87d9e5f40ac5af015b3ad53684b11d7f6039
Edwin <edwinthinks@gmail.com>
2021-12-31 08:53:45 -0500

    Add test coverage for when you use or not use a layout in system tests

--- test/sandbox/app/components/simple_javascript_interaction_component.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class SimpleJavascriptInteractionComponent < ViewComponent::Base
-  def initialize
-  end
-end
--- test/view_component/view_component_system_test.rb
+++ test/view_component/view_component_system_test.rb
@@ -5,11 +5,20 @@ require "test_helper"
 class ViewComponentSystemTest < ViewComponent::SystemTestCase
   driven_by :cuprite
 
-  def test_simple_js_interaction_in_browser
-    visit_rendered_component_in_browser(SimpleJavascriptInteractionComponent.new, layout: "application")
+  def test_simple_js_interaction_in_browser_without_layout
+    visit_rendered_component_in_browser(SimpleJavascriptInteractionWithJsIncludedComponent.new)
 
     assert find("[data-hidden-field]", visible: false)
     find("[data-button]", text: "Click Me To Reveal Something Cool").click
     assert find("[data-hidden-field]", visible: true)
   end
+
+  def test_simple_js_interaction_in_browser_with_layout
+    visit_rendered_component_in_browser(SimpleJavascriptInteractionWithoutJsIncludedComponent.new, layout: 'application')
+
+    assert find("[data-hidden-field]", visible: false)
+    find("[data-button]", text: "Click Me To Reveal Something Cool").click
+    assert find("[data-hidden-field]", visible: true)
+  end
+
 end
