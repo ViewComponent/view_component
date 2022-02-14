30edbb7946042951dada1da58bd3919fabf1c295
Elia Schito <elia@schito.me>
2022-02-14 11:44:02 +0100

    Add deprecation horizon management to the release script

--- lib/view_component/deprecation.rb
+++ lib/view_component/deprecation.rb
@@ -3,5 +3,6 @@
 require "active_support/deprecation"
 
 module ViewComponent
-  Deprecation = ActiveSupport::Deprecation.new("3.0", "ViewComponent")
+  DEPRECATION_HORIZON = 3
+  Deprecation = ActiveSupport::Deprecation.new(DEPRECATION_HORIZON.to_s, "ViewComponent")
 end
