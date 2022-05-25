45daf39cbdf18834c8fea1bb01101584bfe3f336
Joel Hawksley <joel@hawksley.org>
2022-05-25 16:05:33 -0600

    try this

--- test/sandbox/test/components/my_component_test.rb
+++ test/sandbox/test/components/my_component_test.rb
@@ -6,6 +6,7 @@ class MyComponentTest < ViewComponent::TestCase
   include ViewComponent::RenderPreviewHelper
 
   def test_render_preview
+    Rails.autoloaders.main.push_dir(Rails.root.join("test/components/previews"))
     render_preview(:default)
 
     assert_selector("div", text: "hello,world!")
