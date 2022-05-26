ffc855fa0c47aca3faaed4224370c02eb93d8458
Joel Hawksley <joel@hawksley.org>
2022-05-26 17:08:14 -0600

    this one?

--- test/sandbox/test/integration_test.rb
+++ test/sandbox/test/integration_test.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class IntegrationTest < ActionDispatch::IntegrationTest
   def setup
-    # ViewComponent::Preview.load_previews
+    ViewComponent::Preview.load_previews
   end
 
   def test_rendering_component_in_a_view
