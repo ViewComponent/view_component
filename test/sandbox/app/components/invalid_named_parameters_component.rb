80c5a34527efbaf6b3f92e79ba126a93656fddd4
Joel Hawksley <joel@hawksley.org>
2022-07-06 12:01:30 -0600

    rubocop:disable Style/RedundantInitialize

--- test/sandbox/app/components/invalid_named_parameters_component.rb
+++ test/sandbox/app/components/invalid_named_parameters_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class InvalidNamedParametersComponent < ViewComponent::Base
+  # rubocop:disable Style/RedundantInitialize
   def initialize(content:)
   end
+  # rubocop:enable Style/RedundantInitialize
 end
--- test/sandbox/app/components/missing_collection_parameter_name_component.rb
+++ test/sandbox/app/components/missing_collection_parameter_name_component.rb
@@ -3,6 +3,8 @@
 class MissingCollectionParameterNameComponent < ViewComponent::Base
   with_collection_parameter :foo
 
+  # rubocop:disable Style/RedundantInitialize
   def initialize(bar:)
   end
+  # rubocop:enable Style/RedundantInitialize
 end
