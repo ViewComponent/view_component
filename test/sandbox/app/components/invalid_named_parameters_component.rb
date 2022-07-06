013ae03d0cb478a7196860a827eee35d3b0b3a84
Joel Hawksley <joel@hawksley.org>
2022-07-06 12:17:41 -0600

    fix last lints

--- test/sandbox/app/components/invalid_named_parameters_component.rb
+++ test/sandbox/app/components/invalid_named_parameters_component.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class InvalidNamedParametersComponent < ViewComponent::Base
-  # rubocop:disable Style/RedundantInitialize
   def initialize(content:)
   end
-  # rubocop:enable Style/RedundantInitialize
 end
--- test/sandbox/app/components/invalid_parameters_component.rb
+++ test/sandbox/app/components/invalid_parameters_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class InvalidParametersComponent < ViewComponent::Base
+  # rubocop:disable Style/RedundantInitialize
   def initialize(content)
   end
+  # rubocop:enable Style/RedundantInitialize
 end
