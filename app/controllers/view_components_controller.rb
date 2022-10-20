4983d1f3bab11e5ea96dc8a37ef4aca5bc5f8fab
Edwin <edwinthinks@gmail.com>
2022-10-20 09:48:02 -0500

    Move the system_test_entrypoint to the PreviewsAction module

--- app/controllers/view_components_controller.rb
+++ app/controllers/view_components_controller.rb
@@ -4,10 +4,4 @@ require "rails/application_controller"
 
 class ViewComponentsController < Rails::ApplicationController # :nodoc:
   include ViewComponent::PreviewActions
-
-  if Rails.env.test?
-    def system_test_entrypoint
-      render file: "./tmp/view_components/#{params.permit(:file)[:file]}"
-    end
-  end
 end
