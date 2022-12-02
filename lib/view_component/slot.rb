08f7b6d09e31e71bc8ab61990dfed6f2f598c0ad
Joel Hawksley <joel@hawksley.org>
2022-12-02 13:18:20 -0700

    Remove deprecated slots implementation (#1596)
    * BREAKING: Remove deprecated SlotsV1 in favor of current SlotsV2.
    
    * clean up CI

--- lib/view_component/slot.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module ViewComponent
-  class Slot
-    attr_accessor :content
-  end
-end
--- test/sandbox/config/routes.rb
+++ test/sandbox/config/routes.rb
@@ -4,7 +4,6 @@ Sandbox::Application.routes.draw do
   root to: "integration_examples#index"
   get :slots, to: "integration_examples#slots"
   get :empty_slot, to: "integration_examples#empty_slot"
-  get :empty_slot_v2_with_slim, to: "integration_examples#empty_slot_v2_with_slim"
   get :partial, to: "integration_examples#partial"
   get :content, to: "integration_examples#content"
   get :variants, to: "integration_examples#variants"
