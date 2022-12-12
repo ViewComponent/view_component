112e67092fb52232639e533fa1ac2ba9aa30dc2a
Joel Hawksley <joel@hawksley.org>
2022-12-12 10:31:35 -0700

    simplify test helper

--- lib/view_component/system_test_helpers.rb
+++ lib/view_component/system_test_helpers.rb
@@ -10,14 +10,14 @@ module ViewComponent
     # @param layout [String] The (optional) layout to use.
     # @return [Proc] A block that can be used to visit the path of the inline rendered component.
     def with_rendered_component_path(fragment, layout: false, &block)
-      html = controller.render_to_string(html: fragment.to_html.html_safe, layout: layout)
-
-      # Add './tmp/view_components/' directory if it doesn't exist to store the rendered component html
+      # Add './tmp/view_components/' directory if it doesn't exist to store the rendered component HTML
       FileUtils.mkdir_p("./tmp/view_components/") unless Dir.exist?("./tmp/view_components/")
 
       file = Tempfile.new(["rendered_#{fragment.class.name}", ".html"], "tmp/view_components/")
       begin
-        file.write(html)
+        file.write(
+          controller.render_to_string(html: fragment.to_html.html_safe, layout: layout)
+        )
         file.rewind
 
         filename = file.path.split("/").last
