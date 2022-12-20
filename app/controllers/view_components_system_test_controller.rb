dd87156db61b7a1023619c20a2d5d1436f8b6f51
Edwin <edwinthinks@gmail.com>
2022-12-20 10:07:13 -0500

    Fix lint errors

--- app/controllers/view_components_system_test_controller.rb
+++ app/controllers/view_components_system_test_controller.rb
@@ -3,9 +3,7 @@
 require "rails/application_controller"
 
 class ViewComponentsSystemTestController < Rails::ApplicationController # :nodoc:
-
   def system_test_entrypoint
     render file: "./tmp/view_components/#{params.permit(:file)[:file]}"
   end
-
 end
