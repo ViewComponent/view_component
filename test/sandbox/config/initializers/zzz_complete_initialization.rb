223c55d925abc2cc212852f405f7058c41614005
Jon Rohan <rohan@github.com>
2023-01-11 15:34:19 -0800

    Revert "Avoid loading ActionView::Base during initialization (#1528)" (#1626)
    * Revert "Avoid loading ActionView::Base during initialization (#1528)"
    
    This reverts commit 22dbe8f648b44cc73d9663082a3e5024345b28ee.
    
    * Put back changelog
    
    * Adding changelog for revert

--- test/sandbox/config/initializers/zzz_complete_initialization.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# FRAMEWORK_LOAD_POINTS ought to be empty - we shouldn't have
-# autoloaded ActionView::Base during initialization, for example.
-FRAMEWORK_LOAD_POINTS.each do |framework, caller|
-  warn "#{framework} loaded too soon, from:"
-  warn caller
-  exit 1
-end
