0ee2df726d4756fee182576323b53931d5428aa0
Blake Williams <blake@blakewilliams.me>
2023-02-01 14:06:13 -0500

    Add basic module doc

--- lib/view_component/capture_compatibility.rb
+++ lib/view_component/capture_compatibility.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 module ViewComponent
+  # CaptureCompatibility is a module that patches #capture to fix issues
+  # related to ViewComponent and functionality that relies on `capture`
+  # like forms, capture itself, turbo frames, etc.
+  #
+  # This underlying incompatibility with ViewComponent and capture is
+  # that several features like forms keep a reference to the primary
+  # `ActionView::Base` instance which has its own @output_buffer. When
+  # `#capture` is called on the original `ActionView::Base` instance while
+  # evaluating a block from a ViewComponent the @output_buffer is overridden
+  # in the ActionView::Base instance, and *not* the component. This results
+  # in a double render due to `#capture` implementation details.
+  #
+  # To resolve the issue, we override `#capture` so that we can delegate
+  # the `capture` logic to the ViewComponent that created the block.
   module CaptureCompatibility
     def self.included(base)
       base.class_eval do
