6b994c664e67f6dd87f8e4fe441a3657c7b45927
Nick Malcolm <nmalcolm@gitlab.com>
2023-02-21 11:59:33 +1300

    Protect against Arbitrary File Read in System Test Controller

--- app/controllers/view_components_system_test_controller.rb
+++ app/controllers/view_components_system_test_controller.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
 class ViewComponentsSystemTestController < ActionController::Base # :nodoc:
+  BASE_VIEW_COMPONENT_PATH = ::File.realpath("./tmp/view_components/").freeze
+
+  before_action :validate_test_env
+  before_action :validate_file_path
+
   def system_test_entrypoint
-    render file: "./tmp/view_components/#{params.permit(:file)[:file]}"
+    render file: @path
+  end
+
+  private
+
+  def validate_test_env
+    raise "ViewComponentsSystemTestController must only be called in a test environment" unless Rails.env.test?
+  end
+
+  # Ensure that the file path is valid and doesn't target files outside
+  # the expected directory (e.g. via a path traversal or symlink attack)
+  def validate_file_path
+    @path = ::File.realpath(params.permit(:file)[:file], BASE_VIEW_COMPONENT_PATH)
+    unless @path.start_with?(BASE_VIEW_COMPONENT_PATH)
+      raise ArgumentError, "Invalid file path"
+    end
   end
 end
