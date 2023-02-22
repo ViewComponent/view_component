68c23ec1612c7ed4dc4c3bfe59b11ec11278effa
Joel Hawksley <joel@hawksley.org>
2023-02-22 10:16:37 -0700

    even more DRY!

--- app/controllers/view_components_system_test_controller.rb
+++ app/controllers/view_components_system_test_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ViewComponentsSystemTestController < ActionController::Base # :nodoc:
-  TEMP_DIR = "./tmp/view_components/"
+  TEMP_DIR = FileUtils.mkdir_p("./tmp/view_components/").first
 
   before_action :validate_test_env
   before_action :validate_file_path
@@ -19,8 +19,6 @@ class ViewComponentsSystemTestController < ActionController::Base # :nodoc:
   # Ensure that the file path is valid and doesn't target files outside
   # the expected directory (e.g. via a path traversal or symlink attack)
   def validate_file_path
-    FileUtils.mkdir_p(TEMP_DIR)
-
     base_path = ::File.realpath(TEMP_DIR).freeze
     @path = ::File.realpath(params.permit(:file)[:file], base_path)
     unless @path.start_with?(base_path)
