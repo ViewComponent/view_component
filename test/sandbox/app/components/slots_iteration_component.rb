793c31116f8b03dae1226653ffd6b75e646d39b9
reeganviljoen <reeganviljoen@outlook.com>
2023-05-20 20:33:20 +0200

    fixed linter errors

--- test/sandbox/app/components/slots_iteration_component.rb
+++ test/sandbox/app/components/slots_iteration_component.rb
@@ -2,14 +2,14 @@
 
 class SlotsIterationComponent < ViewComponent::Base
   renders_many :numbered_tabs, NumberedTabComponent
-  renders_many :numbered_items, 'NumberedItemComponent'
+  renders_many :numbered_items, "NumberedItemComponent"
 
   def initialize(classes: "")
     @classes = classes
   end
 
   class NumberedItemComponent < ViewComponent::Base
-    delegate :first? , :last?, to: :item_iteration
+    delegate :first?, :last?, to: :item_iteration
     attr_accessor :item_number, :item_iteration, :title
 
     def initialize(title:, numbered_item_counter:, numbered_item_iteration:)
