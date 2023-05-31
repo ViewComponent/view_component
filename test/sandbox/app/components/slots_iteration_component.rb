718af5d90154541f7e8eec1f338fa45536f47624
Joel Hawksley <joel@hawksley.org>
2023-05-31 13:11:12 -0600

    only attr_reader is necessary

--- test/sandbox/app/components/slots_iteration_component.rb
+++ test/sandbox/app/components/slots_iteration_component.rb
@@ -5,7 +5,7 @@ class SlotsIterationComponent < ViewComponent::Base
   renders_many :numbered_items, "NumberedItemComponent"
 
   class NumberedItemComponent < ViewComponent::Base
-    attr_accessor :item_number, :item_iteration, :title
+    attr_reader :item_number, :item_iteration, :title
 
     def initialize(title:, numbered_item_counter:, numbered_item_iteration:)
       @item_number = numbered_item_counter
