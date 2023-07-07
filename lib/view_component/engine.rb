dd05dc8bb646f801509020fe9215d5ae8a265dad
Travis Gaff <travis@labzero.com>
2023-07-07 12:27:35 +0900

    lint

--- lib/view_component/engine.rb
+++ lib/view_component/engine.rb
@@ -40,7 +40,7 @@ module ViewComponent
         Base.config = app.config.view_component
 
         if app.config.view_component.instrumentation_enabled.present?
-          # :nocov: Re-executing the below in a test duplicates initializers and causes order-dependent failures.
+          # :nocov: Re-executing the below in tests duplicates initializers and causes order-dependent failures.
           ViewComponent::Base.prepend(ViewComponent::Instrumentation)
           if app.config.view_component.use_deprecated_instrumentation_name
             ViewComponent::Deprecation.deprecation_warning(
@@ -48,7 +48,7 @@ module ViewComponent
               "Use the new instrumentation key `render.view_component` instead. See https://viewcomponent.org/guide/instrumentation.html"
             )
           end
-         # :nocov:
+          # :nocov:
         end
       end
     end
