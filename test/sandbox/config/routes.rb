30b6dd2ad1e18a532e07593c18bb43cb4638e549
Andrew Duthie <andrew.duthie@gsa.gov>
2023-07-18 15:34:46 -0400

    Allow setting method when using the  test helper

--- test/sandbox/config/routes.rb
+++ test/sandbox/config/routes.rb
@@ -29,6 +29,7 @@ Sandbox::Application.routes.draw do
   get :cached_partial, to: "integration_examples#cached_partial"
   get :inherited_sidecar, to: "integration_examples#inherited_sidecar"
   get :inherited_from_uncompilable_component, to: "integration_examples#inherited_from_uncompilable_component"
+  post :create, to: "integration_examples#create"
 
   constraints(lambda { |request| request.env["warden"].authenticate! }) do
     get :constraints_with_env, to: "integration_examples#index"
