11e3aefae315f50ff7d8bb24c84c79550c06a996
reeganviljoen <reeganviljoen@outlook.com>
2023-09-25 21:09:17 +0200

    add: args and block passthrough

--- lib/view_component/helpers_api.rb
+++ lib/view_component/helpers_api.rb
@@ -4,8 +4,8 @@ module ViewComponent::HelpersApi
   class_methods do
     def use_helper(*args)
       args.each do |helper|
-        define_method helper do
-          helpers.send(helper)
+        define_method helper do |*args, &block|
+          helpers.send(helper, *args, &block)
         end
       end
     end
