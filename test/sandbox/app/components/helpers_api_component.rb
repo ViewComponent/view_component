b06db0453077ca5c7f57156785281a95d71a63dd
Reegan Viljoen <62689748+reeganviljoen@users.noreply.github.com>
2023-09-29 13:50:24 +0200

    Update test/sandbox/app/components/helpers_api_component.rb
    Co-authored-by: Cameron Dutro <camertron@gmail.com>

--- test/sandbox/app/components/helpers_api_component.rb
+++ test/sandbox/app/components/helpers_api_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class HelpersApiComponent < ViewComponent::Base
-  include ViewComponent::HelpersApi
+class UseHelperComponent < ViewComponent::Base
+  include ViewComponent::UseHelpers
 
   use_helper :message
 end
