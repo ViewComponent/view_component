77e3b6c47a504b4cb5cefd466396f5913447a030
Cameron Dutro <camertron@gmail.com>
2023-11-03 11:32:23 -0700

    Remove unused test component; add test that verifies methods can start with 'call' (but not 'call_')

--- test/sandbox/app/components/header_component.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class HeaderComponent < ViewComponent::Base
-  renders_one :callout_title
-end
--- test/sandbox/test/slotable_test.rb
+++ test/sandbox/test/slotable_test.rb
@@ -720,7 +720,7 @@ class SlotableTest < ViewComponent::TestCase
     assert component.title.content?
   end
 
-  def test_slot_names_can_not_start_with_call_
+  def test_slot_names_cannot_start_with_call_
     assert_raises ViewComponent::InvalidSlotNameError do
       Class.new(ViewComponent::Base) do
         renders_one :call_out_title
@@ -730,6 +730,14 @@ class SlotableTest < ViewComponent::TestCase
     assert_raises ViewComponent::InvalidSlotNameError do
       Class.new(ViewComponent::Base) do
         renders_many :call_out_titles
+      end
+    end
+  end
+
+  def test_slot_names_can_start_with_call
+    assert_nothing_raised do
+      Class.new(ViewComponent::Base) do
+        renders_one :callhome_et
       end
     end
   end
