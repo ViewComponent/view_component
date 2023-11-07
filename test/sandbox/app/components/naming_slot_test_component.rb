b65f807c37de83d979d4d48c975cbff29f4a67ba
Hugo Chantelauze <93914147+hchtlz@users.noreply.github.com>
2023-11-07 15:33:35 +0100

    Apply suggestions from code review
    Co-authored-by: Hans Lemuet <Spone@users.noreply.github.com>

--- test/sandbox/app/components/naming_slot_test_component.rb
+++ test/sandbox/app/components/naming_slot_test_component.rb
@@ -2,6 +2,8 @@ class NamingSlotTestComponent < ViewComponent::Base
   renders_many :series, "Serie"
 
   class Serie < ViewComponent::Base
+    attr_reader :name
+
     def initialize(name:)
       @name = name
     end
