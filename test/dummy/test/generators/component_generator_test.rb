e187e1b387d397f96f02fb2dc595476503be4ed1
Tomasz Kowalewski <me@tkowalewski.pl>
2024-01-17 19:45:47 +0100

    Fix tests

--- test/dummy/test/generators/component_generator_test.rb
+++ test/dummy/test/generators/component_generator_test.rb
@@ -3,12 +3,8 @@
 require_relative "../../test_helper"
 require "rails/generators/component/component_generator"
 
-Dummy::Engine.load_generators
-
 class ComponentGeneratorTest < Rails::Generators::TestCase
   tests Rails::Generators::ComponentGenerator
-  # destination Dummy::Engine.root
-
   destination Dir.mktmpdir
   setup :prepare_destination
 
@@ -17,7 +13,7 @@ class ComponentGeneratorTest < Rails::Generators::TestCase
   def test_component
     run_generator
 
-    assert_file "app/components/user_component.rb" do |component|
+    assert_file "app/components/dummy/user_component.rb" do |component|
       assert_match(/module Dummy/, component)
       assert_match(/class UserComponent < ViewComponent::Base/, component)
       assert_no_match(/def initialize/, component)
