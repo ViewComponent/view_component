fc125ca623ed2c7794d89b0bedcdd11f4649bc7d
Tomasz Kowalewski <me@tkowalewski.pl>
2024-01-17 19:14:07 +0100

    Allow to run tests with 'm' ;)

--- test/dummy/test/generators/component_generator_test.rb
+++ test/dummy/test/generators/component_generator_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "../../test_helper"
 require "rails/generators/component/component_generator"
 
 Dummy::Engine.load_generators
