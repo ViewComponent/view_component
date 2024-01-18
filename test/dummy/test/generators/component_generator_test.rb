614c09f134cda3462f0bd331a38d08487e0cbb78
Tomasz Kowalewski <me@tkowalewski.pl>
2024-01-18 21:25:29 +0100

    One step closer with tests

--- test/dummy/test/generators/component_generator_test.rb
+++ test/dummy/test/generators/component_generator_test.rb
@@ -8,35 +8,13 @@ class ComponentGeneratorTest < Rails::Generators::TestCase
   destination Dir.mktmpdir
   setup :prepare_destination
 
-  arguments %w[user]
-
   def test_component
-    run_generator
+    run_generator %w[example]
 
-    assert_file "app/components/dummy/user_component.rb" do |component|
+    assert_file "app/components/dummy/example_component.rb" do |component|
       assert_match(/module Dummy/, component)
-      assert_match(/class UserComponent < ViewComponent::Base/, component)
+      assert_match(/class ExampleComponent < ViewComponent::Base/, component)
       assert_no_match(/def initialize/, component)
-    end
-  end
-
-  def test_component_tests
-    run_generator %w[user --test-framework test_unit]
-
-    assert_file "test/components/dummy/user_component_test.rb" do |component|
-      assert_match(/module Dummy/, component)
-      assert_match(/class UserComponentTest < /, component)
-      assert_match(/def test_component_renders_something_useful/, component)
-    end
-  end
-
-  def test_component_preview
-    run_generator %w[user --preview]
-
-    assert_file "test/components/previews/dummy/user_component_preview.rb" do |component|
-      assert_match(/module Dummy/, component)
-      assert_match(/class UserComponentPreview < /, component)
-      assert_match(/render\(UserComponent.new\)/, component)
     end
   end
 end
