1bb5d9f4d493af2e18f676958fa49b8c191f2146
Reegan Viljoen <62689748+reeganviljoen@users.noreply.github.com>
2024-01-22 21:09:57 +0200

    Update test/sandbox/app/components/use_helpers_component.rb
    Co-authored-by: Joel Hawksley <joelhawksley@github.com>

--- test/sandbox/app/components/use_helpers_component.rb
+++ test/sandbox/app/components/use_helpers_component.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 class UseHelpersComponent < ViewComponent::Base
-  # include ViewComponent::UseHelpers
-  # This module is now part of ViewComponent::Base, but the tests that
-  # use this component are testing the functionality the module provides.
   use_helpers :message
 end
