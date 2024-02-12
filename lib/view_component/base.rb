d7d684fa0307ecc79d8cffe721038c57ffedd4a5
Reegan Viljoen <reegan.viljoen@platform45.com>
2024-02-12 22:49:02 +0200

    fix tests

--- lib/view_component/base.rb
+++ lib/view_component/base.rb
@@ -44,7 +44,6 @@ module ViewComponent
     VC_INTERNAL_DEFAULT_FORMAT = :html
 
     # For CSRF authenticity tokens in forms and Content Security Policy nonces
-    use_helpers :form_authenticity_token, :protect_against_forgery?, :config, :content_security_policy_nonce
 
     attr_accessor :__vc_original_view_context
 
@@ -71,7 +70,19 @@ module ViewComponent
     # @return [String]
     def render_in(view_context, &block)
       self.class.compile(raise_errors: true)
+      if ViewComponent::Base.config.strict_helpers_enabled
+        class_eval(<<~RUBY, __FILE__, __LINE__ + 1)
+          use_helpers :form_authenticity_token, :protect_against_forgery?, :config, :content_security_policy_nonce
+        RUBY
+      else
+        class_eval(<<~RUBY, __FILE__, __LINE__ + 1)
+          # For CSRF authenticity tokens in forms
+          delegate :form_authenticity_token, :protect_against_forgery?, :config, to: :helpers
 
+          # For Content Security Policy nonces
+          delegate :content_security_policy_nonce, to: :helpers
+        RUBY
+      end
       @view_context = view_context
       self.__vc_original_view_context ||= view_context
 
