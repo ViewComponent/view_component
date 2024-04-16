86fad7b749bb9bdb3b3d46b32a42433ea03dfd30
Cameron Dutro <camertron@gmail.com>
2024-04-16 09:52:34 -0700

    Prevent warnings

--- test/sandbox/app/components/forwarding_slot_wrapper_component.rb
+++ test/sandbox/app/components/forwarding_slot_wrapper_component.rb
@@ -3,34 +3,36 @@
 class ForwardingSlotWrapperComponent < ViewComponent::Base
 end
 
-class ForwardingSlotComponent < ViewComponent::Base
-  def initialize
-    @target = TargetSlotComponent.new
+unless defined?(ForwardingSlotComponent)
+  class ForwardingSlotComponent < ViewComponent::Base
+    def initialize
+      @target = TargetSlotComponent.new
+    end
+
+    def with_target_content(&block)
+      @target.with_target_content(&block)
+    end
+
+    def before_render
+      content
+    end
+
+    def call
+      render(@target)
+    end
   end
 
-  def with_target_content(&block)
-    @target.with_target_content(&block)
+  class TargetSlotComponent < ViewComponent::Base
+    renders_one :target_content, "TargetContentComponent"
+
+    def call
+      target_content
+    end
   end
 
-  def before_render
-    content
-  end
-
-  def call
-    render(@target)
-  end
-end
-
-class TargetSlotComponent < ViewComponent::Base
-  renders_one :target_content, "TargetContentComponent"
-
-  def call
-    target_content
-  end
-end
-
-class TargetContentComponent < ViewComponent::Base
-  def call
-    content
+  class TargetContentComponent < ViewComponent::Base
+    def call
+      content
+    end
   end
 end
