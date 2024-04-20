f82de1d9fab2eb7e3a999e23190b394def473d83
Reegan Viljoen <reegan.viljoen@platform45.com>
2024-04-20 11:11:11 +0200

    add: inline benchmark

---
+++ performance/inline_benchmark.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Run `bundle exec rake benchmark` to execute benchmark.
+# This is very much a work-in-progress. Please feel free to make/suggest improvements!
+
+require "benchmark/ips"
+
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "production"
+require File.expand_path("../test/sandbox/config/environment.rb", __dir__)
+
+module Performance
+  require_relative "components/inline_trailing_whitespace_component"
+end
+
+class BenchmarksController < ActionController::Base
+end
+
+BenchmarksController.view_paths = [File.expand_path("./views", __dir__)]
+controller_view = BenchmarksController.new.view_context
+
+Benchmark.ips do |x|
+  x.time = 10
+  x.warmup = 2
+
+  x.report("strip_trailing_white_space") do
+    controller_view.render(Performance::InlineTrailingWhitespaceComponent.new)
+  end
+
+  x.compare!
+end
