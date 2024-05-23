d4c41b643497fb3ff86f5b5e487b7bb3747dc161
Reegan Viljoen <reegan.viljoen@platform45.com>
2024-05-23 13:44:09 +0200

    fix liniting

--- test/sandbox/test/rendering_test.rb
+++ test/sandbox/test/rendering_test.rb
@@ -563,7 +563,7 @@ class RenderingTest < ViewComponent::TestCase
         render_inline(ExceptionInTemplateComponent.new)
       end
 
-    component_error_index = Rails::VERSION::STRING < '8.0' ? 0 : 1
+    component_error_index = (Rails::VERSION::STRING < "8.0") ? 0 : 1
     assert_match %r{app/components/exception_in_template_component\.html\.erb:2}, error.backtrace[component_error_index]
   end
 
