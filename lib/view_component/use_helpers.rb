04a1ab064115493efcb278db43e56a66afa22aed
Reegan Viljoen <reegan.viljoen@platform45.com>
2024-05-26 21:52:14 +0200

    refactor helpers macro

--- lib/view_component/use_helpers.rb
+++ lib/view_component/use_helpers.rb
@@ -6,22 +6,34 @@ module ViewComponent::UseHelpers
   class_methods do
     def use_helpers(*args, from: nil)
       helper_source = from
-      args.each do |helper_method|
-        if helper_source.nil?
-          class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
-            def #{helper_method}(*args, &block)
-              raise HelpersCalledBeforeRenderError if view_context.nil?
-              __vc_original_view_context.#{helper_method}(*args, &block)
-            end
-          RUBY
-        else
-          class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
-            def #{helper_method}(*args, &block)
-              #{helper_source}.instance_method(:#{helper_method}).bind(self).call(*args, &block)
-            end
-          RUBY
-        end
+      if helper_source.nil?
+        define_helpers_without_source(*args)
+      else
+        define_helpers_with_source(*args, source: helper_source)
+      end
+    end
 
+    private
+
+    def define_helpers_without_source(*args)
+      args.each do |helper_method|
+        class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
+          def #{helper_method}(*args, &block)
+            raise HelpersCalledBeforeRenderError if view_context.nil?
+            __vc_original_view_context.#{helper_method}(*args, &block)
+          end
+        RUBY
+        ruby2_keywords(helper_method) if respond_to?(:ruby2_keywords, true)
+      end
+    end
+
+    def define_helpers_with_source(*args, source:)
+      args.each do |helper_method|
+        class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
+          def #{helper_method}(*args, &block)
+            #{source}.instance_method(:#{helper_method}).bind(self).call(*args, &block)
+          end
+        RUBY
         ruby2_keywords(helper_method) if respond_to?(:ruby2_keywords, true)
       end
     end
