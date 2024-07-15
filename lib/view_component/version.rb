e5c4a3e5f8a7eabb52993ae8ae63f6399e162628
Joel Hawksley <joelhawksley@github.com>
2024-07-15 14:11:50 -0600

    release v3.13.0

--- lib/view_component/version.rb
+++ lib/view_component/version.rb
@@ -3,8 +3,8 @@
 module ViewComponent
   module VERSION
     MAJOR = 3
-    MINOR = 12
-    PATCH = 1
+    MINOR = 13
+    PATCH = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join(".")
