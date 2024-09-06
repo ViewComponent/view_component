e53d8a3d1715a353b4a890a2bc5e412aec147d4b
Joel Hawksley <joelhawksley@github.com>
2024-09-06 10:28:40 -0600

    remove attr_reader

--- lib/view_component/compiler.rb
+++ lib/view_component/compiler.rb
@@ -20,31 +20,31 @@ module ViewComponent
     end
 
     def compiled?
-      CompileCache.compiled?(component)
+      CompileCache.compiled?(@component)
     end
 
     def compile(raise_errors: false, force: false)
-      return if (compiled? && !force) || component == ViewComponent::Base
+      return if (compiled? && !force) || @component == ViewComponent::Base
 
       gather_templates
 
-      if self.class.mode == DEVELOPMENT_MODE && templates.none? { !(_1.inline_call? && !_1.defined_on_self?) }
-        component.superclass.compile(raise_errors: raise_errors)
+      if self.class.mode == DEVELOPMENT_MODE && @templates.none? { !(_1.inline_call? && !_1.defined_on_self?) }
+        @component.superclass.compile(raise_errors: raise_errors)
       end
 
       return if gather_template_errors(raise_errors).any?
 
       if raise_errors
-        component.validate_initialization_parameters!
-        component.validate_collection_parameter!
+        @component.validate_initialization_parameters!
+        @component.validate_collection_parameter!
       end
 
       define_render_template_for
 
-      component.register_default_slots
-      component.build_i18n_backend
+      @component.register_default_slots
+      @component.build_i18n_backend
 
-      CompileCache.register(component)
+      CompileCache.register(@component)
     end
 
     # TODO this should probably take format into account
@@ -54,18 +54,18 @@ module ViewComponent
 
     private
 
-    attr_reader :component, :templates
+    attr_reader :templates
 
     def define_render_template_for
-      templates.each { _1.compile_to_component(@redefinition_lock) }
+      @templates.each { _1.compile_to_component(@redefinition_lock) }
 
       method_body =
-        if (template = templates.find { _1.inline? })
+        if (template = @templates.find { _1.inline? })
           template.safe_method_name
         else
           branches = []
 
-          templates.each do |template|
+          @templates.each do |template|
             conditional =
               if template.inline_call?
                 "variant&.to_sym == #{template.variant.inspect}"
@@ -90,8 +90,8 @@ module ViewComponent
         end
 
       @redefinition_lock.synchronize do
-        component.silence_redefinition_of_method(:render_template_for)
-        component.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+        @component.silence_redefinition_of_method(:render_template_for)
+        @component.class_eval <<-RUBY, __FILE__, __LINE__ + 1
         def render_template_for(variant = nil, format = nil)
           #{method_body}
         end
@@ -102,12 +102,12 @@ module ViewComponent
     def gather_template_errors(raise_errors)
       errors = []
 
-      errors << "Couldn't find a template file or inline render method for #{component}." if templates.empty?
+      errors << "Couldn't find a template file or inline render method for #{@component}." if @templates.empty?
 
       # We currently allow components to have both an inline call method and a template for a variant, with the
       # inline call method overriding the template. We should aim to change this in v4 to instead
       # raise an error.
-      templates.select { !_1.inline_call? }
+      @templates.select { !_1.inline_call? }
         .map { |template| [template.variant, template.format] }
         .tally
         .select { |_, count| count > 1 }
@@ -116,20 +116,20 @@ module ViewComponent
 
         variant_string = " for variant `#{variant}`" if variant.present?
 
-        errors << "More than one #{this_format.upcase} template found#{variant_string} for #{component}. "
+        errors << "More than one #{this_format.upcase} template found#{variant_string} for #{@component}. "
       end
 
-      if templates.any? { _1.variant.nil? && !_1.inline_call? } &&
-          templates.any? { _1.variant.nil? && _1.inline_call? && _1.defined_on_self? }
+      if @templates.any? { _1.variant.nil? && !_1.inline_call? } &&
+          @templates.any? { _1.variant.nil? && _1.inline_call? && _1.defined_on_self? }
 
         errors <<
-          "Template file and inline render method found for #{component}. " \
+          "Template file and inline render method found for #{@component}. " \
           "There can only be a template file or inline render method per component."
       end
 
       duplicate_template_file_and_inline_call_variants =
-        templates.select { !_1.inline_call? }.map(&:variant) &
-        templates.select { _1.inline_call? && _1.defined_on_self? }.map(&:variant)
+        @templates.select { !_1.inline_call? }.map(&:variant) &
+        @templates.select { _1.inline_call? && _1.defined_on_self? }.map(&:variant)
 
       unless duplicate_template_file_and_inline_call_variants.empty?
         count = duplicate_template_file_and_inline_call_variants.count
@@ -138,11 +138,11 @@ module ViewComponent
           "Template #{"file".pluralize(count)} and inline render #{"method".pluralize(count)} " \
           "found for #{"variant".pluralize(count)} " \
           "#{duplicate_template_file_and_inline_call_variants.map { |v| "'#{v}'" }.to_sentence} " \
-          "in #{component}. There can only be a template file or inline render method per variant."
+          "in #{@component}. There can only be a template file or inline render method per variant."
       end
 
       variant_pairs =
-        templates.select { _1.variant.present? }.map { [_1.variant, _1.normalized_variant_name] }.uniq { _1.first }
+        @templates.select { _1.variant.present? }.map { [_1.variant, _1.normalized_variant_name] }.uniq { _1.first }
 
       colliding_normalized_variants =
         variant_pairs.map(&:last).tally.select { |_, count| count > 1 }.keys
@@ -153,7 +153,7 @@ module ViewComponent
         end
 
       colliding_normalized_variants.each do |variants|
-        errors << "Colliding templates #{variants.sort.map { |v| "'#{v}'" }.to_sentence} found in #{component}."
+        errors << "Colliding templates #{variants.sort.map { |v| "'#{v}'" }.to_sentence} found in #{@component}."
       end
 
       raise TemplateError.new(errors) if errors.any? && raise_errors
@@ -164,13 +164,13 @@ module ViewComponent
     def gather_templates
       @templates ||=
         begin
-          templates = component.sidecar_files(
+          templates = @component.sidecar_files(
             ActionView::Template.template_handler_extensions
           ).map do |path|
             pieces = File.basename(path).split(".")
 
             out = Template.new(
-              component: component,
+              component: @component,
               type: :file,
               path: path,
               lineno: 0,
@@ -185,28 +185,28 @@ module ViewComponent
           end
 
           (
-            component.ancestors.take_while { |ancestor| ancestor != ViewComponent::Base } - component.included_modules
+            @component.ancestors.take_while { |ancestor| ancestor != ViewComponent::Base } - @component.included_modules
           ).flat_map { |ancestor| ancestor.instance_methods(false).grep(/^call(_|$)/) }
             .uniq
             .each do |method_name|
               templates << Template.new(
-                component: component,
+                component: @component,
                 type: :inline_call,
                 this_format: :html,
                 variant: method_name.to_s.include?("call_") ? method_name.to_s.sub("call_", "").to_sym : nil,
                 method_name: method_name,
-                defined_on_self: component.instance_methods(false).include?(method_name)
+                defined_on_self: @component.instance_methods(false).include?(method_name)
               )
             end
 
-          if component.inline_template.present?
+          if @component.inline_template.present?
             templates << Template.new(
-              component: component,
+              component: @component,
               type: :inline,
-              path: component.inline_template.path,
-              lineno: component.inline_template.lineno,
-              source: component.inline_template.source.dup,
-              extension: component.inline_template.language
+              path: @component.inline_template.path,
+              lineno: @component.inline_template.lineno,
+              source: @component.inline_template.source.dup,
+              extension: @component.inline_template.language
             )
           end
 
