5052b30c04c75d4d965056150f6bae82a184addc
Simon Fish <si@mon.fish>
2024-10-08 14:54:34 +0100

    Lint

--- lib/view_component/global_config.rb
+++ lib/view_component/global_config.rb
@@ -1,3 +1,3 @@
 module ViewComponent
-  GlobalConfig = defined?(Rails) && Rails.application ? Rails.application.config.view_component : Base.config
+  GlobalConfig = (defined?(Rails) && Rails.application) ? Rails.application.config.view_component : Base.config # standard:disable Naming/ConstantName
 end
