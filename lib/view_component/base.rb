a24c0c46cf7c2d637c9def32eda4bac4686e8af8
Joel Hawksley <joel@hawksley.org>
2024-10-16 08:47:26 -0600

    Update lib/view_component/base.rb

--- lib/view_component/base.rb
+++ lib/view_component/base.rb
@@ -293,7 +293,7 @@ module ViewComponent
     #
     # @private
     def __vc_request
-      @__request ||= controller.request if controller.respond_to?(:request)
+      @__vc_request ||= controller.request if controller.respond_to?(:request)
     end
 
     # The content passed to the component instance as a block.
