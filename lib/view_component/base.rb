d083bd6cb4246805f557635cbd5a24681af51c62
Reegan Viljoen <reegan.viljoen@platform45.com>
2024-11-05 10:58:05 +0200

    fix

--- lib/view_component/base.rb
+++ lib/view_component/base.rb
@@ -270,6 +270,15 @@ module ViewComponent
     # For caching, such as #cache_if
     #
     # @private
+    def view_cache_dependencies
+      return unless __vc_cache_dependencies.present? && __vc_cache_dependencies.any?
+
+      __vc_cache_dependencies.map { |dep| send(dep) }.compact
+    end
+
+    # For caching, such as #cache_if
+    #
+    # @private
     def format
       @__vc_variant if defined?(@__vc_variant)
     end
@@ -287,15 +296,6 @@ module ViewComponent
     # @private
     def __vc_request
       @__vc_request ||= controller.request if controller.respond_to?(:request)
-    end
-
-    # For caching, such as #cache_if
-    #
-    # @private
-    def view_cache_dependencies
-      return unless __vc_cache_dependencies.present? && __vc_cache_dependencies.any?
-
-      __vc_cache_dependencies.map { |dep| send(dep) }.compact
     end
 
     # The content passed to the component instance as a block.
@@ -460,8 +460,6 @@ module ViewComponent
     # ```ruby
     # config.view_component.generate.preview = true
     # ```
-    #
-    #  Defaults to `false`
     #
 
     class << self
