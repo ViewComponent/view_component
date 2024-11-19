29aa3ad583a0e5c14835f242063a920f6dcedd70
Reegan Viljoen <62689748+reeganviljoen@users.noreply.github.com>
2024-11-19 21:17:06 +0200

    Update test/sandbox/test/rendering_test.rb
    Co-authored-by: Cameron Dutro <camertron@gmail.com>

--- test/sandbox/test/rendering_test.rb
+++ test/sandbox/test/rendering_test.rb
@@ -15,14 +15,12 @@ class RenderingTest < ViewComponent::TestCase
     ViewComponent::CompileCache.cache.delete(MyComponent)
     MyComponent.ensure_compiled
 
-    if Rails.version.to_f >= 8.0
-      assert_allocations("3.4.0" => 109, "3.3.6" => 124, "3.3.0" => 127, "3.2.6" => 114, "3.1.6" => 114, "3.0.7" => 123) do
-        render_inline(MyComponent.new)
-      end
-    else
-      assert_allocations("3.4.0" => 109, "3.3.6" => 115, "3.3.0" => 127, "3.2.6" => 114, "3.1.6" => 114, "3.0.7" => 123) do
-        render_inline(MyComponent.new)
-      end
+    allocations = Rails.version.to_f >= 8.0 ? 
+      { "3.4.0" => 109, "3.3.6" => 124, "3.3.0" => 127, "3.2.6" => 114, "3.1.6" => 114, "3.0.7" => 123 } :
+      { "3.4.0" => 109, "3.3.6" => 115, "3.3.0" => 127, "3.2.6" => 114, "3.1.6" => 114, "3.0.7" => 123 }
+
+    assert_allocations(**allocations) do
+      render_inline(MyComponent.new)
     end
 
     assert_selector("div", text: "hello,world!")
