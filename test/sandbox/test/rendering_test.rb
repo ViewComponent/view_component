4c9cf7ed24abc0dcb8eeb885d9f74c5b5fb98137
Joel Hawksley <joelhawksley@github.com>
2025-01-07 15:35:36 -0700

    resolve warnings in tests

--- test/sandbox/test/rendering_test.rb
+++ test/sandbox/test/rendering_test.rb
@@ -964,11 +964,13 @@ class RenderingTest < ViewComponent::TestCase
   end
 
   def test_concurrency_deadlock_cache
-    with_compiler_development_mode(true) do
-      with_new_cache do
-        render_inline(ContentEvalComponent.new) do
-          ViewComponent::CompileCache.invalidate!
-          render_inline(ContentEvalComponent.new)
+    assert_nothing_raised do
+      with_compiler_development_mode(true) do
+        with_new_cache do
+          render_inline(ContentEvalComponent.new) do
+            ViewComponent::CompileCache.invalidate!
+            render_inline(ContentEvalComponent.new)
+          end
         end
       end
     end
@@ -1089,27 +1091,29 @@ class RenderingTest < ViewComponent::TestCase
   end
 
   def test_concurrency_deadlock
-    with_compiler_development_mode(true) do
-      with_new_cache do
-        mutex = Mutex.new
+    assert_nothing_raised do
+      with_compiler_development_mode(true) do
+        with_new_cache do
+          mutex = Mutex.new
 
-        t1 = Thread.new do
-          mutex.synchronize do
-            sleep 0.02
-            render_inline(ContentEvalComponent.new)
-          end
-        end
-
-        t = Thread.new do
-          render_inline(ContentEvalComponent.new) do
+          t1 = Thread.new do
             mutex.synchronize do
-              sleep 0.01
+              sleep 0.02
+              render_inline(ContentEvalComponent.new)
             end
           end
-        end
 
-        t1.join
-        t.join
+          t = Thread.new do
+            render_inline(ContentEvalComponent.new) do
+              mutex.synchronize do
+                sleep 0.01
+              end
+            end
+          end
+
+          t1.join
+          t.join
+        end
       end
     end
   end
