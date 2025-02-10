ffb02e3fa528da91bb4eaa900f7d1f672b917b65
Tiago Garcia <tiagomenegazmg@gmail.com>
2025-02-09 19:51:25 -0800

    Fix broken tests

--- test/sandbox/test/rendering_test.rb
+++ test/sandbox/test/rendering_test.rb
@@ -16,7 +16,7 @@ class RenderingTest < ViewComponent::TestCase
     MyComponent.ensure_compiled
 
     allocations = (Rails.version.to_f >= 8.0) ?
-      {"3.5.0" => 104, "3.4.1" => 104, "3.3.7" => 108} :
+      {"3.5.0" => 102, "3.4.1" => 104, "3.3.7" => 108} :
       {"3.3.7" => 107, "3.3.0" => 120, "3.2.7" => 105, "3.1.6" => 118, "3.0.7" => 127}
 
     assert_allocations(**allocations) do
