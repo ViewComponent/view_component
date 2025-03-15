58ffa749fae2039536479cba66a7ee60aaa001af
Simon Fish <si@mon.fish>
2025-03-15 12:49:15 +0000

    Introduce component-local config

---
+++ lib/view_component/component_local_config.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module ViewComponent
+  module ComponentLocalConfig
+    class Configuration
+      def initialize
+        @config = ActiveSupport::OrderedOptions[]
+      end
+
+      delegate_missing_to :@config
+
+      def inheritable_copy
+        new.instance_variable_set(:@config, @config.inheritable_copy)
+      end
+    end
+
+    extend ActiveSupport::Concern
+
+    included do
+      def configuration
+        @_configuration ||= self.class.configuration.inheritable_copy
+      end
+
+      private
+
+      def inherited(child)
+        child.instance_variable_set(:@_configuration, nil)
+        super
+      end
+    end
+
+    class_methods do
+      def configuration
+        @_configuration ||= if respond_to?(:superclass) && superclass.respond_to?(:configuration)
+          superclass.configuration.inheritable_copy
+        else
+          # create a new "anonymous" class that will host the compiled reader methods
+          Class.new(ActiveSupport::Configurable::Configuration).new
+        end
+      end
+
+      def configure(&block)
+        configuration.instance_eval(&block)
+        configuration.compile_methods!
+      end
+    end
+  end
+end
