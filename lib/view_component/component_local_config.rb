f2171ce5691a909b4dfd0e92307c290acc5f6e63
Simon Fish <si@mon.fish>
2025-03-20 15:46:21 +0000

    Rename interface methods

--- lib/view_component/component_local_config.rb
+++ lib/view_component/component_local_config.rb
@@ -28,32 +28,32 @@ module ViewComponent
 
     included do
       # :nocov:
-      def configuration
-        @_configuration ||= self.class.configuration.inheritable_copy
+      def view_component_config
+        @__vc_config ||= self.class.view_component_config.inheritable_copy
       end
 
       private
 
       def inherited(child)
-        child.instance_variable_set(:@_configuration, nil)
+        child.instance_variable_set(:@__vc_config, nil)
         super
       end
       # :nocov:
     end
 
     class_methods do
-      def configuration
-        @_configuration ||= if respond_to?(:superclass) && superclass.respond_to?(:configuration)
-          superclass.configuration.inheritable_copy
+      def view_component_config
+        @__vc_config ||= if respond_to?(:superclass) && superclass.respond_to?(:view_component_config)
+          superclass.view_component_config.inheritable_copy
         else
           # create a new "anonymous" class that will host the compiled reader methods
           ViewComponent::ComponentLocalConfig::Configuration.new
         end
       end
 
-      def configure(&block)
-        configuration.instance_eval(&block)
-        configuration.compile_methods!
+      def configure_component(&block)
+        view_component_config.instance_eval(&block)
+        view_component_config.compile_methods!
       end
     end
   end
