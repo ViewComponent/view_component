027a721186bb82baa1a63c88a6d9ec4dd616b372
Joel Hawksley <joelhawksley@github.com>
2025-03-31 13:56:09 -0600

    remove unused code not caught due to nocov

--- lib/view_component/errors.rb
+++ lib/view_component/errors.rb
@@ -218,24 +218,6 @@ module ViewComponent
       "`#controller` to a [`#before_render` method](https://viewcomponent.org/api.html#before_render--void)."
   end
 
-  # :nocov:
-  class NoMatchingTemplatesForPreviewError < StandardError
-    MESSAGE = "Found 0 matches for templates for TEMPLATE_IDENTIFIER."
-
-    def initialize(template_identifier)
-      super(MESSAGE.gsub("TEMPLATE_IDENTIFIER", template_identifier))
-    end
-  end
-
-  class MultipleMatchingTemplatesForPreviewError < StandardError
-    MESSAGE = "Found multiple templates for TEMPLATE_IDENTIFIER."
-
-    def initialize(template_identifier)
-      super(MESSAGE.gsub("TEMPLATE_IDENTIFIER", template_identifier))
-    end
-  end
-  # :nocov:
-
   class SystemTestControllerOnlyAllowedInTestError < BaseError
     MESSAGE = "ViewComponent SystemTest controller must only be called in a test environment for security reasons."
   end
