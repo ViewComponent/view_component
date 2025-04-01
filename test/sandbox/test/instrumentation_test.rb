dc5783bc5b34875a60c0cb8403b9d7b857468189
Joel Hawksley <joelhawksley@github.com>
2025-04-01 13:50:07 -0600

    improve test coverage

--- test/sandbox/test/instrumentation_test.rb
+++ test/sandbox/test/instrumentation_test.rb
@@ -3,11 +3,12 @@
 require "test_helper"
 
 class InstrumentationTest < ViewComponent::TestCase
-  def test_instrumentation
+  def test_instrumentation_for_include
     events = []
     ActiveSupport::Notifications.subscribe("render.view_component") do |*args|
       events << ActiveSupport::Notifications::Event.new(*args)
     end
+
     render_inline(InstrumentationComponent.new)
 
     assert_selector("div", text: "hello,world!")
@@ -15,5 +16,19 @@ class InstrumentationTest < ViewComponent::TestCase
     assert_equal("render.view_component", events[0].name)
     assert_equal(events[0].payload[:name], "InstrumentationComponent")
     assert_match("app/components/instrumentation_component.rb", events[0].payload[:identifier])
+  end
+
+  def test_instrumentation_disabled
+    with_instrumentation_enabled_option(false) do
+      events = []
+      ActiveSupport::Notifications.subscribe("render.view_component") do |*args|
+        events << ActiveSupport::Notifications::Event.new(*args)
+      end
+
+      render_inline(InstrumentationComponent.new)
+
+      assert_selector("div", text: "hello,world!")
+      assert_equal(events.size, 0)
+    end
   end
 end
