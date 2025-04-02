e0d0ad984a18e927a5307ebc0f689ee9301319c6
Joel Hawksley <joelhawksley@github.com>
2025-04-02 09:45:10 -0600

    enable reloading

--- test/sandbox/test/rendering_test.rb
+++ test/sandbox/test/rendering_test.rb
@@ -435,7 +435,7 @@ class RenderingTest < ViewComponent::TestCase
   def test_compiles_unrendered_component
     # The UnreferencedComponent will get compiled at boot,
     # but that might have been thrown away if code-reloading is enabled
-    skip unless Rails.application.config.cache_classes
+    skip if Rails.application.config.enable_reloading
 
     assert UnreferencedComponent.compiled?
   end
@@ -443,7 +443,7 @@ class RenderingTest < ViewComponent::TestCase
   def test_compiles_components_without_initializers
     # MissingInitializerComponent will get compiled at boot,
     # but that might have been thrown away if code-reloading is enabled
-    skip unless Rails.application.config.cache_classes
+    skip if Rails.application.config.enable_reloading
 
     assert MissingInitializerComponent.compiled?
   end
