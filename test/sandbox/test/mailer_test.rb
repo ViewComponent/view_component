11a2c62e4ed3645e4a0459b0381750d93e93c1d3
Joel Hawksley <joelhawksley@github.com>
2025-04-07 13:10:47 -0600

    attempt to reproduce bug #2267

--- test/sandbox/test/mailer_test.rb
+++ test/sandbox/test/mailer_test.rb
@@ -2,8 +2,15 @@
 
 require "test_helper"
 
-class MailerTest < ViewComponent::TestCase
+class MailerTest < ActionMailer::TestCase
   def test_rendering_component_in_an_action_mailer
-    assert_includes TestMailer.test_email.deliver_now.body.raw_source, "<div>Hello world!</div>"
+    result = TestMailer.test_email.deliver_now.body.to_s
+    assert_includes result, "<div>Hello world!</div>"
+    assert_includes result, "test_email.html.erb"
+  end
+
+  def test_rendering_component_in_an_action_mailer_with_asset_component
+    result = TestMailer.test_asset_email.deliver_now.body.to_s
+    assert_includes result, "<div>/assets"
   end
 end
