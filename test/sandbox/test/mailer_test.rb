7be29b1f974fec61e146c6c160c6af7cf82b4024
Joel Hawksley <joel@hawksley.org>
2025-04-29 16:21:39 -0600

    Update test/sandbox/test/mailer_test.rb

--- test/sandbox/test/mailer_test.rb
+++ test/sandbox/test/mailer_test.rb
@@ -4,7 +4,6 @@ require "test_helper"
 
 class MailerTest < ViewComponent::TestCase
   def test_rendering_component_in_an_action_mailer
-    puts RUBY_VERSION
     assert_includes TestMailer.test_email.deliver_now.body.raw_source, "<div>Hello world!</div>"
   end
 end
