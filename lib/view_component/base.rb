fcdfee6df80a4d3b933ac95e69df81128ffced1a
reeganviljoen <reeganviljoen@outlook.com>
2025-05-02 22:13:57 +0200

    clean up stuf added in last rebase

--- lib/view_component/base.rb
+++ lib/view_component/base.rb
@@ -51,10 +51,6 @@ module ViewComponent
     # For Content Security Policy nonces
     delegate :content_security_policy_nonce, to: :helpers
 
-    # Config option that strips trailing whitespace in templates before compiling them.
-    class_attribute :__vc_strip_trailing_whitespace, instance_accessor: false, instance_predicate: false
-    self.__vc_strip_trailing_whitespace = false # class_attribute:default doesn't work until Rails 5.2
-
     attr_accessor :__vc_original_view_context
 
     # Components render in their own view context. Helpers and other functionality
