03683fecb4190b7c52f281f6ea0ed73d7487b043
reeganviljoen <reeganviljoen@outlook.com>
2025-05-06 22:47:46 +0200

    namespace registry

--- lib/view_component/cache_registry.rb
+++ lib/view_component/cache_registry.rb
@@ -1,16 +1,19 @@
-module CachingRegistry # :nodoc:
+  
+module ViewComponent
+  module CachingRegistry # :nodoc:
     extend self
 
     def caching?
-      ActiveSupport::IsolatedExecutionState[:action_view_caching] ||= false
+      ActiveSupport::IsolatedExecutionState[:view_component_caching] ||= false
     end
 
     def track_caching
-      caching_was = ActiveSupport::IsolatedExecutionState[:action_view_caching]
+      caching_was = ActiveSupport::IsolatedExecutionState[:view_component_caching]
       ActiveSupport::IsolatedExecutionState[:action_view_caching] = true
 
       yield
     ensure
-      ActiveSupport::IsolatedExecutionState[:action_view_caching] = caching_was
+      ActiveSupport::IsolatedExecutionState[:view_component_caching] = caching_was
     end
+  end
 end
