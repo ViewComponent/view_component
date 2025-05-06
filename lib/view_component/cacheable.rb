750d1d3f965a03cd4f8444f89b483aae462fc6eb
reeganviljoen <reeganviljoen@outlook.com>
2025-05-06 22:43:39 +0200

    Add cache refistry and alighn cache with how action view does it

--- lib/view_component/cacheable.rb
+++ lib/view_component/cacheable.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'set'
+require 'view_component/cache_registry'
 
 module ViewComponent::Cacheable
   extend ActiveSupport::Concern
@@ -15,7 +16,7 @@ module ViewComponent::Cacheable
       return if __vc_cache_dependencies.blank? || __vc_cache_dependencies.none? || __vc_cache_dependencies.nil?
 
       computed_view_cache_dependencies = __vc_cache_dependencies.map { |dep| if respond_to?(dep) then public_send(dep) end }
-      ActiveSupport::Cache.expand_cache_key(computed_view_cache_dependencies)
+      combined_fragment_cache_key(ActiveSupport::Cache.expand_cache_key(computed_view_cache_dependencies))
     end
 
     # Render component from cache if possible
@@ -23,12 +24,41 @@ module ViewComponent::Cacheable
     # @private
     def __vc_render_cacheable(rendered_template)
       if __vc_cache_dependencies != [:format, :__vc_format]
-        Rails.cache.fetch(view_cache_dependencies) do
-          __vc_render_template(rendered_template)
+        CachingRegistry.track_caching do
+           template_fragment(rendered_template)
         end
       else
         __vc_render_template(rendered_template)
       end
+    end
+
+    def template_fragment(rendered_template)
+      if content = read_fragment(rendered_template)
+        @view_renderer.cache_hits[@current_template&.virtual_path] = :hit if defined?(@view_renderer)
+        content
+      else
+        @view_renderer.cache_hits[@current_template&.virtual_path] = :miss if defined?(@view_renderer)
+        write_fragment(rendered_template)
+      end
+    end
+
+    def read_fragment(rendered_template)
+      Rails.cache.fetch(view_cache_dependencies) 
+    end
+
+    def write_fragment(rendered_template)
+      content = __vc_render_template(rendered_template)
+      Rails.cache.fetch(view_cache_dependencies) do
+        content
+      end
+      content
+    end
+
+    def combined_fragment_cache_key(key)
+      cache_key = [:view_component, ENV["RAILS_CACHE_ID"] || ENV["RAILS_APP_VERSION"], key]
+      cache_key.flatten!(1)
+      cache_key.compact!
+      cache_key
     end
   end
 
