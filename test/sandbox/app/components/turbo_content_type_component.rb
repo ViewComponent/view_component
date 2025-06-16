ecb33b9cdcca38c85a6ec538bc9ff52d42fcc12e
Joel Hawksley <joelhawksley@github.com>
2025-06-16 13:47:40 -0600

    inline helper method

--- test/sandbox/app/components/turbo_content_type_component.rb
+++ test/sandbox/app/components/turbo_content_type_component.rb
@@ -7,15 +7,4 @@ class TurboContentTypeComponent < ViewComponent::Base
   private
 
   attr_reader :message, :show_form
-
-  def form_content
-    form_tag("/submit", method: :post, data: {turbo_frame: "test-frame"}) do
-      content_tag(:div) do
-        content_tag(:h2, @message) +
-          text_field_tag("user_message", "", value: "Test message") +
-          content_tag(:br) +
-          submit_tag("Submit", id: "submit")
-      end
-    end
-  end
 end
