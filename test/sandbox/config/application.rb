351fc8908c568e94408c23cf09d136d0c56c8f4d
Joel Hawksley <joelhawksley@github.com>
2025-06-18 13:43:03 -0600

    address rails warning differently

--- test/sandbox/config/application.rb
+++ test/sandbox/config/application.rb
@@ -37,11 +37,9 @@ require "jbuilder"
 
 module Sandbox
   class Application < Rails::Application
-    config.action_controller.asset_host = "http://assets.example.com"
+    config.load_defaults(8.1) if Rails::VERSION::MAJOR == 8 && Rails::VERSION::MINOR == 1
 
-    if config.action_controller.respond_to?(:escape_json_responses=)
-      config.action_controller.escape_json_responses = false
-    end
+    config.action_controller.asset_host = "http://assets.example.com"
 
     # Prepare test_set_no_duplicate_autoload_paths
     config.autoload_paths.push("#{config.root}/my/components/previews")
