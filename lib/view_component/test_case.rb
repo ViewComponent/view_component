2ac0becd84bfc88c73d061675160fef909b24c42
Joel Hawksley <joelhawksley@github.com>
2025-06-19 16:05:48 -0600

    add docs for ViewComponent::TestCase

--- lib/view_component/test_case.rb
+++ lib/view_component/test_case.rb
@@ -3,6 +3,7 @@
 require "active_support/test_case"
 
 module ViewComponent
+  # Base test case for unit tests of ViewComponents. Includes `ViewComponent::TestHelpers`.
   class TestCase < ActiveSupport::TestCase
     include ViewComponent::TestHelpers
   end
