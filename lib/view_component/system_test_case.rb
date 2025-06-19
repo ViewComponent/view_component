cf8dc48a416f09297dfd55819c26bf35324e3c01
Joel Hawksley <joelhawksley@github.com>
2025-06-19 16:13:01 -0600

    add docs for SystemTestCase

--- lib/view_component/system_test_case.rb
+++ lib/view_component/system_test_case.rb
@@ -3,9 +3,11 @@
 require "active_support/test_case"
 
 module ViewComponent
+  # Base test case for system tests of ViewComponents. Includes `ViewComponent::SystemTestHelpers`.
   class SystemTestCase < ActionDispatch::SystemTestCase
     include ViewComponent::SystemTestHelpers
 
+    # Set `page` for Capybara to visit
     def page
       Capybara.current_session
     end
