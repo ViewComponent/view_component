5a2dfbc671b957c556023995037690e6be558fea
Joel Hawksley <joelhawksley@github.com>
2025-06-19 16:01:15 -0600

    add docs for ViewComponent::Version

--- lib/view_component/version.rb
+++ lib/view_component/version.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module ViewComponent
-  module VERSION
-    MAJOR = 4
-    MINOR = 0
-    PATCH = 0
-    PRE = "alpha7"
+  module VERSION # The semantic version of the gem
+    MAJOR = 4 # The major version of the gem
+    MINOR = 0 # The minor version of the gem
+    PATCH = 0 # The patch version of the gem
+    PRE = "alpha7" # The prerelease version of the gem
 
-    STRING = [MAJOR, MINOR, PATCH, PRE].compact.join(".")
+    STRING = [MAJOR, MINOR, PATCH, PRE].compact.join(".") # The semantic version of the gem in string format
   end
 end
 
