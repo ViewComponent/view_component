5298474bbc792c1d45574b040063f2d28a61a71e
Joel Hawksley <joelhawksley@github.com>
2025-06-25 13:55:26 -0600

    automate generating list of contributors

--- script/sync_contributors.rb
+++ script/sync_contributors.rb
@@ -9,18 +9,18 @@ def fetch_contributors
   all_contributors = []
   page = 1
   per_page = 100 # Maximum allowed by GitHub API
-  
+
   loop do
     uri = URI("https://api.github.com/repos/viewcomponent/view_component/contributors")
     uri.query = URI.encode_www_form(page: page, per_page: per_page)
-    
+
     contributors = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
       request = Net::HTTP::Get.new(uri)
       # Add User-Agent header as GitHub API recommends it
       request["User-Agent"] = "ViewComponent Contributors Sync Script"
-      
+
       response = http.request(request)
-      
+
       if response.code == "200"
         JSON.parse(response.body)
       else
@@ -28,41 +28,38 @@ def fetch_contributors
         exit 1
       end
     end
-    
+
     # Break if no more contributors on this page
     break if contributors.empty?
-    
+
     all_contributors.concat(contributors)
     puts "Fetched page #{page} with #{contributors.length} contributors"
-    
+
     # Break if we got less than the full page size (last page)
     break if contributors.length < per_page
-    
+
     page += 1
   end
-  
+
   all_contributors
 end
 
 # Transform contributors data for YAML output
 def transform_contributors(contributors)
   usernames = contributors.map do |contributor|
-    {
-      "login" => contributor["login"],
-      "html_url" => contributor["html_url"]
-    }
-  end
-  
+    contributor["login"]
+  end.sort
+
   { "usernames" => usernames }
 end
 
 # Write contributors data to YAML file
 def write_contributors_yaml(data)
   output_path = File.join(__dir__, "..", "docs", "_data", "contributors.yml")
-  
+
   # Ensure the directory exists
   FileUtils.mkdir_p(File.dirname(output_path))
-  
+
   File.write(output_path, data.to_yaml)
   puts "Contributors synced to #{output_path}"
   puts "Found #{data['usernames'].length} contributors"
@@ -72,13 +69,13 @@ end
 begin
   puts "Fetching contributors from GitHub API..."
   contributors = fetch_contributors
-  
+
   puts "Transforming contributor data..."
   transformed_data = transform_contributors(contributors)
-  
+
   puts "Writing to YAML file..."
   write_contributors_yaml(transformed_data)
-  
+
   puts "Successfully synced contributors!"
 rescue => e
   puts "Error: #{e.message}"
