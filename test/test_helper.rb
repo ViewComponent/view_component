bb1cd6dcfaa6a3e62fb1d642138e068fe586f79e
Cameron Dutro <camertron@gmail.com>
2025-08-04 22:50:00 -0700

    Ok allocations, knock it off

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -191,5 +191,5 @@ def assert_allocations(count_map, &block)
   total = trace.allocations.all.size
   count = count_map[RUBY_VERSION.split(".").first(2).join(".")]
 
-  assert_equal count, total, "Expected #{count} allocations, got #{total} allocations for Ruby #{RUBY_VERSION}"
+  assert count === total, "Expected #{count} allocations, got #{total} allocations for Ruby #{RUBY_VERSION}"
 end
