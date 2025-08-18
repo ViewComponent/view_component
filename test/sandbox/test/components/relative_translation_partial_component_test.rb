f26924f60bbf368131527647c028fbceb5d5ec2d
LowLevel <kerlqdev@outlook.de>
2025-08-19 00:22:29 +0200

    Remove comments in test

--- test/sandbox/test/components/relative_translation_partial_component_test.rb
+++ test/sandbox/test/components/relative_translation_partial_component_test.rb
@@ -6,11 +6,6 @@ class RelativeTranslationPartialComponentTest < ViewComponent::TestCase
   def test_relative_translation_in_partial_block
     render_inline(RelativeTranslationPartialParentComponent.new)
 
-    # This should pass if Rails resolves the translation key relative to the caller's path,
-    # but currently fails because it resolves relative to the partial's path.
     assert_text "Test Component Title"
-
-    # This assertion documents the current (incorrect) behavior.
-    assert_no_text "Partial Title"
   end
 end
