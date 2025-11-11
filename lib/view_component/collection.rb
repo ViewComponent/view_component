65124c1537d0bce0b3336977fa67fecdf2bae407
Hans Lemuet <hans@etamin.studio>
2025-11-11 15:19:12 +0100

    Standardrb lint

--- lib/view_component/collection.rb
+++ lib/view_component/collection.rb
@@ -5,6 +5,7 @@ require "action_view/renderer/collection_renderer" if Rails.version.to_f >= 6.1
 module ViewComponent
   class Collection
     include Enumerable
+
     attr_reader :component
 
     delegate :size, to: :@collection
