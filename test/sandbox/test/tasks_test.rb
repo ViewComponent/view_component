fbbec1e0fc93e2f7bb346bf474e497fa365be1fd
Hans Lemuet <hans@etamin.studio>
2025-11-11 20:54:08 +0100

    Do not run tasks_test.rb for Rails 8.1

--- test/sandbox/test/tasks_test.rb
+++ test/sandbox/test/tasks_test.rb
@@ -2,21 +2,24 @@
 
 require "test_helper"
 
-class TasksTest < ActiveSupport::TestCase
-  setup do
-    Kernel.silence_warnings do
-      Rails.application.load_tasks
+# `rails stats` command has been removed in Rails 8.1
+if Rails.version.to_f < 8.1
+  class TasksTest < ActiveSupport::TestCase
+    setup do
+      Kernel.silence_warnings do
+        Rails.application.load_tasks
+      end
     end
-  end
 
-  teardown do
-    Rake.application.clear
-  end
+    teardown do
+      Rake.application.clear
+    end
 
-  test "adds components to rails stats" do
-    Dir.chdir(Rails.root) do
-      assert_output(/ViewComponents/) do
-        Rake::Task["stats"].invoke
+    test "adds components to rails stats" do
+      Dir.chdir(Rails.root) do
+        assert_output(/ViewComponents/) do
+          Rake::Task["stats"].invoke
+        end
       end
     end
   end
