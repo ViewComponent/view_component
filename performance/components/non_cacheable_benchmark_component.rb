569fd91c4a6b62a53d4af3de765f9f4537b60ad2
Reegan <reegan@Reegans-MacBook-Air.local>
2026-02-12 15:43:49 +0200

    make benchmark cmponents more expensive too really show ccache performnce

--- performance/components/non_cacheable_benchmark_component.rb
+++ performance/components/non_cacheable_benchmark_component.rb
@@ -7,5 +7,24 @@ module Performance
     def initialize(name:)
       @name = name
     end
+
+    def report_rows
+      @report_rows ||= Array.new(36) do |index|
+        sequence = index + 1
+        amount = ((sequence * 13.75) + (sequence % 4) * 2.125)
+
+        {
+          label: "#{name} item #{sequence}",
+          amount: amount,
+          events: (sequence * 7) % 11 + 1,
+          score: ((sequence * 41) % 100) + 1,
+          slug: "#{name.downcase.tr(" ", "-")}-#{sequence}"
+        }
+      end
+    end
+
+    def total_score
+      report_rows.sum { _1[:score] }
+    end
   end
 end
