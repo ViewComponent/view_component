635645d172cd395f9ca574d0d3edf7c456f43495
Joel Hawksley <joelhawksley@github.com>
2026-05-15 14:21:53 -0600

    fix

--- test/sandbox/test/rendering_test.rb
+++ test/sandbox/test/rendering_test.rb
@@ -20,7 +20,7 @@ class RenderingTest < ViewComponent::TestCase
     MyComponent.__vc_ensure_compiled
 
     with_instrumentation_enabled_option(false) do
-      assert_allocations({"4.1" => 69..70, "4.0" => 69, "3.4" => 75..76, "3.3" => 77..79, "3.2" => 80..82}) do
+      assert_allocations({"4.1" => 69..70, "4.0" => 156, "3.4" => 75..76, "3.3" => 77..79, "3.2" => 80..82}) do
         render_inline(MyComponent.new)
       end
     end
@@ -34,7 +34,7 @@ class RenderingTest < ViewComponent::TestCase
     ViewComponent::CompileCache.cache.delete(ProductComponent)
     ProductComponent.__vc_ensure_compiled
 
-    allocations = {"4.1" => 71, "4.0" => 71, "3.4" => 86..88, "3.3" => 92..94, "3.2" => 97..98}
+    allocations = {"4.1" => 71, "4.0" => 158, "3.4" => 86..88, "3.3" => 92..94, "3.2" => 97..98}
 
     products = [Product.new(name: "Radio clock"), Product.new(name: "Mints")]
     notice = "On sale"
